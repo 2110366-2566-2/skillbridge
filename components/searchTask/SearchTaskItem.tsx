5d774492bd7c7d992a408f2c2da3f35ccc87836f
Awat Singdamrong <a.lubing@yahoo.com>
2024-02-09 02:26:39 +0700

    fix: description's and header's line-height when text is less than 3 and 2 line

--- components/searchTask/SearchTaskItem.tsx
+++ components/searchTask/SearchTaskItem.tsx
@@ -27,7 +27,7 @@ export default function SearchTaskItem() {
             {/* Desktop size */}
             <div className="hidden md:inline-block md:m-[20px] md:border-[0.5px] md:border-slate-500 md:bg-slate-100 md:rounded-xl md:hover:shadow-xl md:hover:cursor-pointer">
                 <div className="flex flex-col justify-between h-[340px] w-[268px] p-4">
-                    <div className="font-bold text-[29px] text-[#313866] text-wrap line-clamp-2">รับสมัคร TA วิชา Com Prog สำหรับน้อง CEDT</div>
+                    <div className="h-[3em] font-bold text-[29px] text-[#313866] text-wrap line-clamp-2">รับสมัคร TA วิชา Com Prog สำหรับน้อง CEDT</div>
                     <div className="font-medium text-[15.5px] text-slate-500 text-wrap line-clamp-1">26/02/67 - 30/02/67</div>
                     <div className="text-[14px] text-slate-800">
                         <span className={`inline-block bg-slate-300 rounded py-1 px-2`}>การสอน</span>
@@ -35,7 +35,7 @@ export default function SearchTaskItem() {
                     <div className="flex flex-col">
                         <div className="font-medium text-[10.5px] text-slate-500">คำอธิบายเกี่ยวกับงาน</div>
                         <hr className="border-slate-300" />
-                        <div className="text-[13px] text-[#838383] text-wrap line-clamp-3 my-[7px]">
+                        <div className="h-[4.5em] text-[13px] text-[#838383] text-wrap line-clamp-3 my-[7px]">
                             รักการสอนเด็ก ๆ, มีความรู้ python numpy, ขยัน ซื่อสัตย์ ประหยัด อดทน ตกน้ำไม่ไหล ตกไฟไม่ไหม้ ยายมีขายหอย ยายมอยขายหมี วันดีคืนดี หมียายมอยไปกัดหอยยายมี ถถถถ 5555
                         </div>
                         <hr className="border-slate-300" />
