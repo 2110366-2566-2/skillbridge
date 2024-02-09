3c5e442170a76400b30fbbfd4c05f57908469c86
naratip <naratip.18257@gmail.com>
2024-02-09 19:25:43 +0700

    update create task

--- actions/create_task.ts
+++ actions/create_task.ts
@@ -1,14 +1,20 @@
 "use server";
 
-import { PrismaClient } from "@prisma/client";
 import prisma from "@/db/prisma";
 
 const createJob = async (fromData: FormData) => {
-  const title = fromData.get("title");
-  const status = fromData.get("status");
-  const description = fromData.get("description");
-  const startDate = fromData.get("startDate");
-  const endDate = fromData.get("endDate");
-  const estimateStartDate = fromData.get("estimateStartDate");
-  const estimateEndDate = fromData.get("estimateEndDate");
+  try {
+    const title = fromData.get("title");
+    const status = fromData.get("status");
+    const description = fromData.get("description");
+    const startDate = fromData.get("startDate");
+    const endDate = fromData.get("endDate");
+    const estimateStartDate = fromData.get("estimateStartDate");
+    const estimateEndDate = fromData.get("estimateEndDate");
+    const budget = fromData.get("budget");
+    const numWorker = fromData.get("NumWorker");
+  } catch (error) {
+    console.log(error);
+    return { message: "Internal Server Error", status: 500 };
+  }
 };
