fd0a35b2a870ae20202b762db25a88af2c7c42e4
Awat Singdamrong <a.lubing@yahoo.com>
2024-02-10 01:57:17 +0700

    feat: usePathName is only show Search and Filter in "/search" (slow)

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import Navbar from "./navbar/Navbar";
 import SearchBar from "./searchAndFilter/SearchBar";
 import Filter from "./searchAndFilter/Filter";
+import SearchAndFilter from "./searchAndFilter/SearchAndFilter";
 
 const whiteLogo = require("@/public/logos/logo-white.svg") as string;
 
@@ -13,52 +14,42 @@ export default async function Header() {
   const isStudent = false;
 
   return (
-    <div>
-      {/* Navbar */}
-      <div className="flex justify-between items-center py-4 pl-3 pr-5 md:pr-10">
-        <Link href="/landing">
-          <Image
-            className="w-auto h-auto md:w-36"
-            src={whiteLogo}
-            alt="logo"
-            width={110}
-            height={110}
-          />
-        </Link>
-        <div className="hidden lg:inline-block">
-          <div className="flex gap-7">
-            <div>
-              <SearchBar />
-            </div>
-            <div>
-              <Filter />
-            </div>
-          </div>
-        </div>
-
-
-        <Navbar session={session} isStudent={isStudent} />
-      </div>
-
+    <>
       {/* Mobile and Tablet */}
-      <div className="flex flex-col lg:hidden">
-        {/* Topic */}
-        <div className="font-semibold text-[30px] text-white ml-5 mb-4">
-          หางาน
+      <div className="lg:hidden">
+        <div className="flex justify-between items-center py-4 pl-3 pr-5 md:pr-10">
+          <Link href="/landing">
+            <Image
+              className="w-auto h-auto md:w-36"
+              src={whiteLogo}
+              alt="logo"
+              width={110}
+              height={110}
+            />
+          </Link>
+          <Navbar session={session} isStudent={isStudent} />
         </div>
-
-        {/* SearchBar and Filter */}
-        <div className="flex flex-row items-center mx-5 mb-5">
-          <div className="w-full mr-[10px]">
-            <SearchBar />
-          </div>
-          <div>
-            <Filter />
-          </div>
-        </div>
+        {/* Search and Filter */}
+        <SearchAndFilter />
       </div>
 
-    </div>
-
+      {/* Desktop */}
+      <div className="hidden lg:inline-block">
+        <div className="flex justify-between items-center py-4 pl-3 pr-5 md:pr-10">
+          <Link href="/landing">
+            <Image
+              className="w-auto h-auto md:w-36"
+              src={whiteLogo}
+              alt="logo"
+              width={110}
+              height={110}
+            />
+          </Link>
+          {/* Search and Filter */}
+          <SearchAndFilter />
+          <Navbar session={session} isStudent={isStudent} />
+        </div>
+      </div>
+    </>
   );
 }
