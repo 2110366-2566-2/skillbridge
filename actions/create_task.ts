807c3976f9e9f1acae11e266654c330c271af698
naratip <naratip.18257@gmail.com>
2024-02-09 14:01:06 +0700

    edit create_task

--- actions/create_task.ts
+++ actions/create_task.ts
@@ -1,15 +1,14 @@
-"use server"
+"use server";
 
-import { PrismaClient } from "@prisma/client"
-import prisma from "@/db/prisma"
+import { PrismaClient } from "@prisma/client";
+import prisma from "@/db/prisma";
 
 const createJob = async (fromData: FormData) => {
-  const title = fromData.get('title')
-  const status = fromData.get('status')
-  const description = fromData.get('description')
-  const startDate = fromData.get('startDate')
-  const endDate = fromData.get('endDate')
-  const estimateStartDate = fromData.get('estimateStartDate')
-  const estimateEndDate = fromData.get('estimateEndDate')
-}
-
+  const title = fromData.get("title");
+  const status = fromData.get("status");
+  const description = fromData.get("description");
+  const startDate = fromData.get("startDate");
+  const endDate = fromData.get("endDate");
+  const estimateStartDate = fromData.get("estimateStartDate");
+  const estimateEndDate = fromData.get("estimateEndDate");
+};
