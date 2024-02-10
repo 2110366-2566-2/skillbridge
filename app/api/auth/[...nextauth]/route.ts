f07c16e7b7038a65af6fa7cf72218b48df39e063
LittleNa1000 <naphanccnp@gmail.com>
2024-02-11 01:29:03 +0700

    add credential provider, refactor RegisterViaEmail

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,13 +1,45 @@
 import NextAuth, { AuthOptions } from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
+import CredentialsProvider from "next-auth/providers/credentials"
+import { PrismaClient } from "@prisma/client/extension"
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import { Adapter } from "next-auth/adapters"
+
+const prisma = new PrismaClient()
 
 export const authOptions: AuthOptions = {
+  adapter: PrismaAdapter(prisma) as Adapter,
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID ?? "",
       clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
     }),
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "email", placeholder: "jsmith" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials, req) {
+        // Add logic here to look up the user from the credentials supplied
+        const user = { id: "1", name: "J Smith", email: "jsmith@example.com" }
+
+        if (user) {
+          // Any object returned will be saved in `user` property of the JWT
+          return user
+        } else {
+          // If you return null then an error will be displayed advising the user to check their details.
+          return null
+
+          // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
+        }
+      },
+    }),
   ],
+  session: {
+    strategy: "jwt",
+  },
+  secret: process.env.NEXTAUTH_SECRET,
   callbacks: {
     signIn: async ({ account, profile }) => {
       if (account?.provider === "google" && profile?.email?.endsWith("21@student.chula.ac.th")) {
--- components/authentication/RegisterViaEmail.tsx
+++ components/authentication/RegisterViaEmail.tsx
@@ -4,89 +4,137 @@ import Link from "next/link"
 import ConfirmPasswordInput from "./ConfirmPasswordInput"
 import { useState } from "react"
 
-export default function RegisterViaEmail({
-    handleFirstFormComplete,
-    isFirstFormComplete
-}: {
-    handleFirstFormComplete: () => void
-    isFirstFormComplete: boolean
-}) {
+type Props = {
+  handleFirstFormComplete: () => void
+  isFirstFormComplete: boolean
+}
 
-    const handleNextPageSubmit = () => {
-        handleFirstFormComplete();
-    };
+export default function RegisterViaEmail({ handleFirstFormComplete, isFirstFormComplete }: Props) {
+  const handleNextPageSubmit = () => {
+    handleFirstFormComplete()
+  }
 
-    const [Form, setForm] = useState({
-        email: '',
-        password: '',
-        cPassword: '',
-        fname: '',
-        lname: ''
+  const [data, setForm] = useState({
+    email: "",
+    password: "",
+    cPassword: "",
+    fname: "",
+    lname: "",
+  })
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setForm({
+      ...data,
+      [event.target.name]: event.target.value,
     })
+    console.log(data)
+  }
 
-    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const handleSubmit = (formData: FormData) => {
+    console.log(data)
+  }
 
-        setForm({
-            ...Form,
-            [event.target.name]: event.target.value
-        })
-        console.log(Form)
-    }
+  return (
+    <form className="w-full" action={handleSubmit}>
+      {!isFirstFormComplete ? (
+        <div className="pt-[5px]">
+          {/* Email Input Component */}
+          <Input
+            name="email"
+            label="อีเมล"
+            inputType="email"
+            warning="กรอกที่อยู่อีเมลของคุณ"
+            handleChange={handleChange}
+            value={data.email}
+          />
 
-    return (
-        <form className="w-full" action={"/"}>
-            {
-                !isFirstFormComplete ?
-                    <div className="pt-[5px]">
-                        {/* Email Input Component */}
-                        <Input name="email" label="อีเมล" inputType="email" warning="กรอกที่อยู่อีเมลของคุณ" handleChange={handleChange} value={Form.email} />
+          {/* Password Input Component */}
+          <PasswordInput fromLoginPage={false} handleChange={handleChange} value={data.password} />
 
-                        {/* Password Input Component */}
-                        <PasswordInput fromLoginPage={false} handleChange={handleChange} value={Form.password} />
+          {/*Confirm Password Input Component */}
+          <ConfirmPasswordInput handleChange={handleChange} value={data.cPassword} />
 
-                        {/*Confirm Password Input Component */}
-                        <ConfirmPasswordInput handleChange={handleChange} value={Form.cPassword} />
+          <div
+            id="nextPage"
+            className="w-full bg-[#334155] text-center cursor-pointer rounded-lg text-white mt-[30px] px-[16px] py-[8px] text-md"
+            onClick={handleNextPageSubmit}>
+            ถัดไป
+          </div>
 
-                        <div id="nextPage" className="w-full bg-[#334155] text-center cursor-pointer rounded-lg text-white mt-[30px] px-[16px] py-[8px] text-md" onClick={handleNextPageSubmit}>
-                            ถัดไป
-                        </div>
+          <p className="w-full text-center text-sm mt-[10px]">
+            มีบัญชีอยู่แล้ว ?{" "}
+            <Link
+              href={"/login"}
+              className="text-[#326FE2] hover:underline hover:underline-offset-2">
+              เข้าสู่ระบบ
+            </Link>
+          </p>
+        </div>
+      ) : (
+        <div className={"pt-[10px] flex flex-col"}>
+          <Input
+            name="fname"
+            label="ชื่อ"
+            inputType="text"
+            warning="กรอกชื่อของคุณ"
+            handleChange={handleChange}
+            value={data.fname}
+          />
+          <Input
+            name="lname"
+            label="นามสกุล"
+            inputType="text"
+            warning="กรอกนามสกุลของคุณ"
+            handleChange={handleChange}
+            value={data.lname}
+          />
 
-                        <p className="w-full text-center text-sm mt-[10px]">
-                            มีบัญชีอยู่แล้ว ? <Link href={"/login"} className="text-[#326FE2] hover:underline hover:underline-offset-2">เข้าสู่ระบบ</Link>
-                        </p>
-
-                    </div>
-                    :
-                    <div className="pt-[10px] flex flex-col">
-                        <Input name="fname" label="ชื่อ" inputType="text" warning="กรอกชื่อของคุณ" handleChange={handleChange} value={Form.fname} />
-                        <Input name="lname" label="นามสกุล" inputType="text" warning="กรอกนามสกุลของคุณ" handleChange={handleChange} value={Form.lname} />
-
-                        <div className="mt-[20px]">
-                            {/* Link ไป ข้อตกลงและเงื่อนไขการใช้งานของ SkillBridge และ นโยบายคุ้มครองความเป็นส่วนตัว*/}
-                            <label className="block relative mb-[10px] text-[9.5px] pl-[20px]">ฉันได้อ่านและยอมรับ<Link href={'/'} className="text-[#326FE2]">ข้อตกลงและเงื่อนไขการใช้งานของ SkillBridge</Link>
-                                <input type="checkbox" name=""
-                                    className="absolute cursor-pointer left-0
+          <div className="mt-[20px]">
+            {/* Link ไป ข้อตกลงและเงื่อนไขการใช้งานของ SkillBridge และ นโยบายคุ้มครองความเป็นส่วนตัว*/}
+            <label className="block relative mb-[10px] text-[9.5px] pl-[20px]">
+              ฉันได้อ่านและยอมรับ
+              <Link href={"/"} className="text-[#326FE2]">
+                ข้อตกลงและเงื่อนไขการใช้งานของ SkillBridge
+              </Link>
+              <input
+                type="checkbox"
+                name=""
+                className="absolute cursor-pointer left-0
                                     checked:bg-[#334155] hover:bg-[#a3a3a3]
                                     w-[14px] h-[14px] rounded-sm focus:outline-none
-                                    hover:ring hover:" />
-                            </label>
+                                    hover:ring hover:"
+              />
+            </label>
 
-                            <label className="block relative text-[9.5px] pl-[20px] ">ฉันได้อ่านและยอมรับ<Link href={'/'} className="text-[#326FE2]">นโยบายคุ้มครองความเป็นส่วนตัว</Link>
-                                <input type="checkbox" name="" className="absolute cursor-pointer left-0 checked:bg-[#334155]" />
-                            </label>
-                        </div>
+            <label className="block relative text-[9.5px] pl-[20px] ">
+              ฉันได้อ่านและยอมรับ
+              <Link href={"/"} className="text-[#326FE2]">
+                นโยบายคุ้มครองความเป็นส่วนตัว
+              </Link>
+              <input
+                type="checkbox"
+                name=""
+                className="absolute cursor-pointer left-0 checked:bg-[#334155]"
+              />
+            </label>
+          </div>
 
-                        <button id="submit" type="submit" className="w-full bg-[#334155] rounded-lg text-white mt-[30px] px-[16px] py-[8px] text-md">
-                            สร้างบัญชี
-                        </button>
+          <button
+            id="submit"
+            type="submit"
+            className="w-full bg-[#334155] rounded-lg text-white mt-[30px] px-[16px] py-[8px] text-md">
+            สร้างบัญชี
+          </button>
 
-                        <button id="previousPage" type="button" className="mt-[20px] text-[#334155] text-md" onClick={handleNextPageSubmit}>
-                            ย้อนกลับ
-                        </button>
-
-                    </div>
-
-            }
-        </form>
-    )
+          <button
+            id="previousPage"
+            type="button"
+            className="mt-[20px] text-[#334155] text-md"
+            onClick={handleNextPageSubmit}>
+            ย้อนกลับ
+          </button>
+        </div>
+      )}
+    </form>
+  )
 }
