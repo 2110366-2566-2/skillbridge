353309e9fda313726f9cc7c90c09c13da02355bf
Awat Singdamrong <a.lubing@yahoo.com>
2024-02-10 19:19:03 +0700

    feat: FormattedDate to xx/xx/xx and handle if date is undefined

--- components/searchTask/SearchTaskItem.tsx
+++ components/searchTask/SearchTaskItem.tsx
@@ -17,26 +17,38 @@ type Props = {
     }
 }
 
+const FormattedDate = (inputDateString: string) => {
+    const [day, month, year] = inputDateString.split('/');
+
+    const formattedYear = year.slice(-2);
+    const formattedDay = day.padStart(2, '0');
+    const formattedMonth = month.padStart(2, '0');
+
+    return `${formattedDay}/${formattedMonth}/${formattedYear}`;
+}
+
 export default function SearchTaskItem({ task }: Props) {
+    console.log(task)
     return (
         <>
             {/* Mobile size */}
             <div className="flex flex-row justify-between items-center min-h-[70px] pb-4 mb-4 border-b border-slate-300 md:hidden">
-                <div className="flex flex-col justify-between">
-                    <div className="font-bold text-[#313866]">ทำ Frontend LMS ชื่อดังในจุฬา</div>
+                <div className="flex flex-col justify-between w-[210.02px]">
+                    <div className="font-bold text-[#313866]">{task?.title}</div>
                     <div className="flex flex-row justify-between">
-                        <div className="font-medium text-[11px] text-slate-500">26/02/67 - 30/02/67</div>
-                        <div className="text-[11px] text-[#838383]">รับแล้ว <span className="font-medium">2 / 10</span> คน</div>
+                        <div className="font-medium text-[11px] text-slate-500">{task?.startDate ? FormattedDate(task.startDate) : "ไม่มีกำหนด"} - {task?.endDate ? FormattedDate(task.endDate) : "ไม่มีกำหนด"}</div>
+                        <div className="text-[11px] text-[#838383]">รับแล้ว <span className="font-medium">{task?.acceptNum} / {task?.maxAcceptNum}</span> คน</div>
                     </div>
                 </div>
                 <div className="flex flex-row justify-end items-center">
-                    <div className="font-semibold text-[20px] text-[#313866] mr-2">฿1,000</div>
+                    <div className="font-semibold text-[20px] text-[#313866] mr-2">฿{task?.price}</div>
                     <Image
                         src={nextArrowButton}
                         alt="nextArrowButton"
                         width={40}
                         height={40}
                         // Open link in new tab
+                        // TODO: Add destination route
                         onClick={() => window.open("/", "_blank", "noopener,noreferrer")}
                         role="link"
                         tabIndex={0}
@@ -48,27 +60,32 @@ export default function SearchTaskItem({ task }: Props) {
             <div
                 className="hidden md:inline-block md:m-[20px] md:border-[0.5px] md:border-slate-200 md:bg-white md:rounded-xl md:hover:shadow-xl md:hover:cursor-pointer"
                 // Open link in new tab
+                // TODO: Add destination route
                 onClick={() => window.open("/", "_blank", "noopener,noreferrer")}
                 role="link"
                 tabIndex={0}
             >
                 <div className="flex flex-col justify-between h-[340px] w-[268px] p-4">
-                    <div className="h-[3em] font-bold text-[29px] text-[#313866] text-wrap line-clamp-2">รับสมัคร TA วิชา Com Prog สำหรับน้อง CEDT</div>
-                    <div className="font-medium text-[15.5px] text-slate-500 text-wrap line-clamp-1">26/02/67 - 30/02/67</div>
-                    <div className="text-[14px] text-slate-800">
-                        <span className={`inline-block bg-slate-200 rounded py-1 px-2`}>การสอน</span>
-                    </div>
+                    <div className="h-[3em] font-bold text-[29px] text-[#313866] text-wrap line-clamp-2">{task?.title}</div>
+                    <div className="font-medium text-[15.5px] text-slate-500 text-wrap line-clamp-1">{task?.startDate ? FormattedDate(task.startDate) : "ไม่มีกำหนด"} - {task?.endDate ? FormattedDate(task.endDate) : "ไม่มีกำหนด"}</div>
+                    {task?.jobTags[0] ? (
+                        <div className="text-[14px] text-slate-800">
+                            <span className={`inline-block bg-slate-200 rounded py-1 px-2`}>{task?.jobTags}</span>
+                        </div>
+                    ) : (
+                        <div className="min-w-[236px] min-h-[29px]"></div>
+                    )}
                     <div className="flex flex-col">
                         <div className="font-medium text-[10.5px] text-slate-500">คำอธิบายเกี่ยวกับงาน</div>
                         <hr className="border-slate-300" />
                         <div className="h-[4.5em] text-[13px] text-[#838383] text-wrap line-clamp-3 my-[7px]">
-                            รักการสอนเด็ก ๆ, มีความรู้ python numpy, ขยัน ซื่อสัตย์ ประหยัด อดทน ตกน้ำไม่ไหล ตกไฟไม่ไหม้ ยายมีขายหอย ยายมอยขายหมี วันดีคืนดี หมียายมอยไปกัดหอยยายมี ถถถถ 5555
+                            {task?.description}
                         </div>
                         <hr className="border-slate-300" />
                     </div>
                     <div className="flex flex-row justify-between items-end translate-y-[5px]">
-                        <div className="text-[13.12px] text-slate-500">รับแล้ว <span className="font-semibold">2 / 10</span> คน</div>
-                        <div className="font-semibold text-[24px] text-[#313866] translate-y-[5px]">฿1,000</div>
+                        <div className="text-[13.12px] text-slate-500">รับแล้ว <span className="font-semibold">{task?.acceptNum} / {task?.maxAcceptNum}</span> คน</div>
+                        <div className="font-semibold text-[24px] text-[#313866] translate-y-[5px]">฿{task?.price}</div>
                     </div>
                 </div>
             </div>
