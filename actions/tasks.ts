a54f8d5c73abdf011bfda59e2708a35c02c9b4de
Fei-7 <supachokang@gmail.com>
2024-02-11 15:03:12 +0700

    fix : getDefaultSearchJobs's output schema

--- actions/tasks.ts
+++ actions/tasks.ts
@@ -2,15 +2,16 @@
 import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient();
 
-interface job {
+export interface job {
+    id: string,
     title: string,
-    startDate: string | undefined,
-    endDate: string | undefined,
-    jobTags: string[],
+    startDate: string,
+    endDate: string,
+    jobTags: string,
     description: string,
     acceptNum: number,
     maxAcceptNum: number,
-    price: number
+    budget: number
 }
 
 async function getDefaultSearchJobs(): Promise<job[]> {
@@ -22,21 +23,22 @@ async function getDefaultSearchJobs(): Promise<job[]> {
             Applied: true
         },
     })
-
+    
     // TODO : Filter for only jobs that are open
     // Waiting for enum to finalize so it can check
     jobs.forEach((job) => {
         const tags = job.jobTags.map((tag) => String(tag.title));
 
         const showJob: job = {
+            id: job.id,
             title: job.title,
-            startDate: job.startDate?.toLocaleDateString('en-GB'),
-            endDate: job.endDate?.toLocaleDateString('en-GB'),
-            jobTags: tags,
+            startDate: job.estimateStartDate.toLocaleDateString('en-GB'),
+            endDate: job.estimateEndDate.toLocaleDateString('en-GB'),
+            jobTags: "-", // Wait for db schema to change
             description: job.description,
             acceptNum: job.Applied.length, //TODO : Filter for accepted application
-            maxAcceptNum: 1000, //TODO : Wait for db schema to change
-            price: 8888 //TODO : Wait for db schema to change
+            maxAcceptNum: job.numWorker,
+            budget: job.budget
         };
 
         output.push(showJob);
@@ -50,6 +52,22 @@ async function getDefaultSearchJobs(): Promise<job[]> {
     })
 
     return output;
+}
+/*
+TODO
+1. write getSearchJobs without filter functionality
+2. add filter functionality
+*/
+async function getSearchJobs(query: string, filter: any): Promise<job[]> {
+    /*
+    get jobs that match query with title
+
+    steps:
+    1. query in elastic search for all the jobs matches
+    2. query in psql to get necessarily details
+    3. format the data for ease of use in frontend page
+    */
+    return [];
 }
 
 /*
