6dfb4120a83f6ff71f79726d8b39dc70ce75b75b
Awat Singdamrong <a.lubing@yahoo.com>
2024-02-12 04:07:48 +0700

    fix: budget show in LocaleString (ex. 10,000)

--- components/searchJob/SearchJobItem.tsx
+++ components/searchJob/SearchJobItem.tsx
@@ -35,14 +35,14 @@ export default function SearchJobItem({ job }: Props) {
             {/* Mobile size */}
             <div className="flex flex-row justify-between items-center min-h-[70px] pb-4 mb-4 border-b border-slate-300 md:hidden">
                 <div className="flex flex-col justify-between w-[210.02px]">
-                    <div className="font-bold text-[#313866] h-[3em] line-clamp-2">{job?.title}</div>
+                    <div className="font-semibold text-[#313866] h-[3em] line-clamp-2">{job?.title}</div>
                     <div className="flex flex-row justify-between">
                         <div className="font-medium text-[11px] text-slate-500">{job?.startDate ? FormattedDate(job.startDate) : "ไม่มีกำหนด"} - {job?.endDate ? FormattedDate(job.endDate) : "ไม่มีกำหนด"}</div>
                         <div className="text-[11px] text-[#838383]">รับแล้ว <span className="font-medium">{job?.acceptNum} / {job?.maxAcceptNum}</span> คน</div>
                     </div>
                 </div>
                 <div className="flex flex-row justify-end items-center">
-                    <div className="font-semibold text-[20px] text-[#313866] mr-2">฿{job?.budget}</div>
+                    <div className="font-semibold text-[20px] text-[#313866] mr-2">฿{job?.budget.toLocaleString()}</div>
                     <Image
                         src={nextArrowButton}
                         alt="nextArrowButton"
@@ -86,7 +86,7 @@ export default function SearchJobItem({ job }: Props) {
                     </div>
                     <div className="flex flex-row justify-between items-end translate-y-[5px]">
                         <div className="text-[13.12px] text-slate-500">รับแล้ว <span className="font-semibold">{job?.acceptNum} / {job?.maxAcceptNum}</span> คน</div>
-                        <div className="font-semibold text-[24px] text-[#313866] translate-y-[5px]">฿{job?.budget}</div>
+                        <div className="font-semibold text-[24px] text-[#313866] translate-y-[5px]">฿{job?.budget.toLocaleString()}</div>
                     </div>
                 </div>
             </div>
