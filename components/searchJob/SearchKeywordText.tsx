d8b3a1e8ee96162acf6780e28e81feb9ecc3d33d
Awat Singdamrong <a.lubing@yahoo.com>
2024-02-12 14:27:45 +0700

    fix: Change Double Quotes to character code

--- components/searchJob/SearchKeywordText.tsx
+++ components/searchJob/SearchKeywordText.tsx
@@ -6,12 +6,12 @@ import Image from "next/image";
 const logoTextBlack = require("@/public/logos/logo-text-black.svg") as string;
 
 export default function SearchKeywordText() {
-    const [keyword, setKeyword] = useState("") // TODO: Make this work using Redux
+    const [keyword, setKeyword] = useState("การสอน") // TODO: Make this work using Redux
 
     return (
         <>
             {keyword ? (
-                <div className="text-3xl text-slate-800 h-[44.15px] translate-y-2">ผลการค้นหาสำหรับ "<span className="font-semibold">{keyword}</span>"</div>
+                <div className="text-3xl text-slate-800 h-[44.15px] translate-y-2">ผลการค้นหาสำหรับ &ldquo;<span className="font-semibold">{keyword}</span>&rdquo;</div>
             ) : (
                 <div className="font-semibold text-3xl text-slate-800">เริ่มต้นหางานฟรีแลนซ์ง่าย ๆ กับ<span> </span>
                     <span className="inline-block translate-y-2">
