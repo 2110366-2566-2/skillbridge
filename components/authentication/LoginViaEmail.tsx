fe5aafcaf5b4470b901391ca713f057cb2d2dad6
Jai <jaijai211075@gmail.com>
2024-02-12 20:35:58 +0700

    feat: merge conflict

--- components/authentication/LoginViaEmail.tsx
+++ components/authentication/LoginViaEmail.tsx
@@ -62,6 +62,8 @@ export default function LoginViaEmail() {
     const handleValidation = (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
         setErrors(validateForm());
+
+
     }
 
     return (
