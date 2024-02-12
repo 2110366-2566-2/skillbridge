aa70b569f56c40939b9f17792c0923715d100888
Fei-7 <supachokang@gmail.com>
2024-02-12 13:27:38 +0700

    feat & fix: add filter functionality to getSearchJobs(query, filter). If you desire to filter search without query, just put 'undefined' for query parameter & export getSearchJobs function because I forgot last commit

--- actions/search/jobs.ts
+++ actions/search/jobs.ts
@@ -27,6 +27,14 @@ export interface job {
     budget: number
 }
 
+export interface jobFilter {
+    startDate?: Date,
+    endDate?: Date,
+    lowestBudget?: number,
+    highestBudget?: number,
+    jobTag?: string
+}
+
 interface elasticJob {
     id: string,
     title: string
@@ -78,7 +86,7 @@ TODO
 1. write getSearchJobs without filter functionality
 2. add filter functionality
 */
-async function getSearchJobs(query: string): Promise<job[]> {
+async function getSearchJobs(query?: string, filter?: jobFilter): Promise<job[]> {
     /*
         Filter is yet to be implemented
     */
@@ -90,6 +98,52 @@ async function getSearchJobs(query: string): Promise<job[]> {
     2. query in psql to get necessarily details
     3. format the data for ease of use in frontend page
     */
+
+    if (query == null) {
+        const output: job[] = [];
+
+        let prismaWhereFromFilter = filter ? getPrismaWhereFromJobFilter(filter) : {};
+
+
+        const jobs = await prisma.job.findMany({
+            include: {
+                jobTag: true,
+                Application: true
+            },
+            where: {
+                status: {
+                    equals: JobStatus.NOT_STARTED
+                },
+                ...prismaWhereFromFilter
+            },
+            take: 15
+        })
+        
+        jobs.forEach((job) => {
+            const showJob: job = {
+                id: job.id,
+                title: job.title,
+                startDate: job.estimateStartDate.toLocaleDateString('en-GB'),
+                endDate: job.estimateEndDate.toLocaleDateString('en-GB'),
+                jobTags: job.jobTag.title, 
+                description: job.description,
+                acceptNum: job.Application.filter(app => app.status==ApplicationStatus.ACCEPTED).length, //TODO : Filter for accepted application
+                maxAcceptNum: job.numWorker,
+                budget: job.budget
+            };
+
+            output.push(showJob);
+        })
+
+        output.sort((a, b) => {
+            const aAcceptRate = a.acceptNum / a.maxAcceptNum;
+            const bAcceptRate = b.acceptNum / b.maxAcceptNum;
+
+            return aAcceptRate - bAcceptRate;
+        })
+
+        return output;
+    }
 
     /* STEP 1 */ 
     const res = await elasticClient.search<elasticJob>({
@@ -104,7 +158,7 @@ async function getSearchJobs(query: string): Promise<job[]> {
                 }
             }
         }
-    })
+    });
 
     // extract jobId from request
     const resJobId = res.hits.hits.map(e => {
@@ -113,6 +167,9 @@ async function getSearchJobs(query: string): Promise<job[]> {
     });
 
     const output: job[] = [];
+
+    let prismaWhereFromFilter = filter ? getPrismaWhereFromJobFilter(filter) : {};
+
 
     const jobs = await prisma.job.findMany({
         include: {
@@ -125,10 +182,11 @@ async function getSearchJobs(query: string): Promise<job[]> {
             },
             id: {
                 in: resJobId
-            }
+            },
+            ...prismaWhereFromFilter
         },
         take: 15
-    })
+    });
     
     jobs.forEach((job) => {
         const showJob: job = {
@@ -144,25 +202,56 @@ async function getSearchJobs(query: string): Promise<job[]> {
         };
 
         output.push(showJob);
-    })
+    });
 
     output.sort((a, b) => {
         const aAcceptRate = a.acceptNum / a.maxAcceptNum;
         const bAcceptRate = b.acceptNum / b.maxAcceptNum;
 
         return aAcceptRate - bAcceptRate;
-    })
+    });
 
     return output;
 }
 
+function getPrismaWhereFromJobFilter(filter: jobFilter) {
+    let prismaWhereFilter: any = {};
 
-// async function main() {
-//     const a = await getSearchJobs("เขียนโปรแกรม");
-//     console.log(a);
-// }
+    if (filter == null) return;
 
-// main();
+    if (filter.startDate != null) 
+        prismaWhereFilter.estimateStartDate = { gte: filter.startDate };
+
+    if (filter.endDate != null) 
+        prismaWhereFilter.estimateEndDate = { lte: filter.endDate };
+
+    if (filter.lowestBudget != null) 
+        prismaWhereFilter.budget = { gte: filter.lowestBudget };
+    
+    if (filter.highestBudget != null) 
+        if (prismaWhereFilter.budget == null) prismaWhereFilter.budget = {};
+        prismaWhereFilter.budget.lte = filter.highestBudget;
+    
+    if (filter.jobTag != null) {
+        prismaWhereFilter.jobTag = { equals: filter.jobTag };
+    }
+
+    return prismaWhereFilter;
+}
+
+/*
+async function main() {
+    const filter: jobFilter = {
+        lowestBudget: 1000+1,
+        highestBudget: 9999
+    };
+
+    const a = await getSearchJobs(undefined, filter);
+    console.log(a);
+}
+
+main();
+*/
 
 
-export { getDefaultSearchJobs };
+export { getDefaultSearchJobs, getSearchJobs };
