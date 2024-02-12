f76d50483b949f53305c5c908961b6a44a6fbef4
Awat Singdamrong <a.lubing@yahoo.com>
2024-02-13 01:26:25 +0700

    feat: Add notFound file

--- components/searchJob/SearchNotFound.tsx
+++ components/searchJob/SearchNotFound.tsx
@@ -1,1 +1,19 @@
-// TODO: Handle when search not found
+import Image from "next/image";
+
+const notFound = require("@/public/logos/notFound.svg") as string;
+
+export default function SearchNotFound() {
+    return (
+        <>
+            {/* Mobile */}
+            <div className="md:hidden">
+                <Image
+                    src={notFound}
+                    alt="notFound"
+                    width={156}
+                    height={156}
+                />
+            </div>
+        </>
+    )
+}
