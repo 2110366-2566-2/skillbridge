82be8a3ade3c8cd50f7cdc2121f5389a42363b35
Awat Singdamrong <a.lubing@yahoo.com>
2024-02-14 22:39:39 +0700

    fix: Delete unuse import

--- app/(withNavigation)/search/page.tsx
+++ app/(withNavigation)/search/page.tsx
@@ -2,21 +2,20 @@ import { authOptions } from "@/app/api/auth/[...nextauth]/auth";
 import SearchJobItemList from "@/components/searchJob/SearchJobItemList";
 import SearchKeywordText from "@/components/searchJob/SearchKeywordText";
 import { getServerSession } from "next-auth";
-import { useSession } from "next-auth/react";
 import { redirect } from "next/navigation";
 import { Suspense } from "react";
 
 export default async function SearchPage() {
   // const {data : session} = useSession();
 
-    // console.log("------------------------------------------------------------")
-    const session = await getServerSession(authOptions) //Server
+  // console.log("------------------------------------------------------------")
+  const session = await getServerSession(authOptions) //Server
 
-    // console.log("session : \n", session);
-    if (!session) {
-      // console.log(".\n.\n.\n.\n.\n.\n.\n.\n.\n.\n.\n.\n.\n.\n.\n.\n")
-      redirect("/login");
-    }
+  // console.log("session : \n", session);
+  if (!session) {
+    // console.log(".\n.\n.\n.\n.\n.\n.\n.\n.\n.\n.\n.\n.\n.\n.\n.\n")
+    redirect("/login");
+  }
 
   return (
     <>
