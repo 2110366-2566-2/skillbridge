90d533aa7e86f6aef769ee42349d706b51bdcf84
Jaiiaj21 <jaijai211075@gmail.com>
2024-02-15 17:27:51 +0700

    feat: register via google toggle page

--- components/authentication/RegisterViaGoogle.tsx
+++ components/authentication/RegisterViaGoogle.tsx
@@ -82,7 +82,134 @@ export default function RegisterViaGoogle({
 
   return (
     <div className="w-full">
+      {
+        !isToggleForm ?
+          <div className="text-[#64748B] leading-6 text-sm w-full">
+            <button
+              className="w-full h-[40px] bg-[#F1F5F9] border border-[#CBD5E1] rounded-md flex items-center px-[20px]"
+              onClick={() =>
+                signIn("google", {
+                  callbackUrl: "/landing",
+                })
+              }
+            >
+              <div>
+                <Image
+                  src={"/logos/google-logo.svg"}
+                  width={20}
+                  height={20}
+                  alt="google logo"
+                />
+              </div>
+              <p className="w-full font-normal font-sm leading-5">
+                เข้าสู่ระบบด้วยบัญชี Google
+              </p>
+            </button>
+          </div>
+          :
+          <form action={handleValidationSecondPage} noValidate>
+            <div className={"pt-[10px] flex flex-col"}>
+              <Input
+                name="fname"
+                label="ชื่อ"
+                inputType="text"
+                warning={errors.fname}
+                handleChange={handleChange}
+                value={data.fname}
+              />
+              <Input
+                name="lname"
+                label="นามสกุล"
+                inputType="text"
+                warning={errors.lname}
+                handleChange={handleChange}
+                value={data.lname}
+              />
 
+              <div className="mt-[30px] w-full relative">
+                {/* Link ไป ข้อตกลงและเงื่อนไขการใช้งานของ SkillBridge และ นโยบายคุ้มครองความเป็นส่วนตัว*/}
+                <input
+                  type="checkbox"
+                  name="checkOne"
+                  id="checkOne"
+                  className="absolute left-0 top-0 border
+                                    border-[#848484]
+                                    accent-[#334155]
+                                    cursor-pointer
+                                    rounded-sm
+                                    "
+                  onChange={(e) => {
+                    handleCheckBoxChange(e);
+                  }}
+                  required
+                />
+                <label
+                  htmlFor="checkOne"
+                  className="block text-[9.5px] pl-[20px] cursor-pointer"
+                >
+                  ฉันได้อ่านและยอมรับ
+                  <Link
+                    href={"/"}
+                    className="text-[#326FE2] hover:underline hover:underline-offset"
+                  >
+                    ข้อตกลงและเงื่อนไขการใช้งานของ SkillBridge
+                  </Link>
+                </label>
+              </div>
+              <div className="mt-[10px] w-full relative">
+                <input
+                  type="checkbox"
+                  name="checkTwo"
+                  id="checkTwo"
+                  className="absolute cursor-pointer left-0 top-0 border
+                                    border-[#848484]
+                                    accent-[#334155]
+                                    rounded-sm
+                                    "
+                  onChange={(e) => {
+                    handleCheckBoxChange(e);
+                  }}
+                  required
+                />
+                <label
+                  htmlFor="checkTwo"
+                  className="block text-[9.5px] pl-[20px] cursor-pointer"
+                >
+                  ฉันได้อ่านและยอมรับ
+                  <Link
+                    href={"/"}
+                    className="text-[#326FE2] hover:underline hover:underline-offset"
+                  >
+                    นโยบายคุ้มครองความเป็นส่วนตัว
+                  </Link>
+                </label>
+              </div>
+
+              {checkBoxError.checkOne && checkBoxError.checkTwo ? (
+                <button
+                  id="submit"
+                  type="submit"
+                  className="w-full bg-[#334155] hover:bg-slate-600 rounded-lg text-white mt-[30px] px-[16px] py-[8px] text-md"
+                >
+                  สร้างบัญชี
+                </button>
+              ) : (
+                <div className="w-full bg-[#CBD5E1] rounded-lg text-white mt-[30px] px-[16px] py-[8px] text-md text-center cursor-pointer">
+                  สร้างบัญชี
+                </div>
+              )}
+
+              <div id="previousPage" className="mt-[15px] flex justify-center">
+                <p
+                  onClick={handleToggleForm}
+                  className="hover:underline hover:underline-offset text-[#334155] hover:text-slate-600 text-md cursor-pointer"
+                >
+                  ย้อนกลับ
+                </p>
+              </div>
+            </div>
+          </form>
+      }
     </div>
 
   )
--- components/authentication/StudentRegister.tsx
+++ components/authentication/StudentRegister.tsx
@@ -1,5 +1,4 @@
 import Title from "./Title"
-import LoginViaGoogle from "./LoginViaGoogle"
 import { useState } from "react"
 import RegisterViaGoogle from "./RegisterViaGoogle"
 
@@ -12,10 +11,11 @@ export default function StudentRegister() {
 
   return (
     <div className="flex flex-col w-[280px] mt-[30px]">
+      <Title title="สร้างบัญชีสำหรับ" highlightText="นิสิต" highlightColor="#FF66C4" />
       <p className="mt-[22px] text-sm">ใช้อีเมลจุฬาฯ เพื่อยืนยันสถานภาพการเป็นนิสิต</p>
 
       <div className="mt-[25px] text-[#64748B] leading-6 text-sm w-full">
-        <LoginViaGoogle />
+        <RegisterViaGoogle handleToggleForm={handleToggleForm} isToggleForm={isToggleForm} />
       </div>
     </div>
   )
