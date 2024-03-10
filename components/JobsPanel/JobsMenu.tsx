0d39da791d37ae7a7e4cf64f645fc6eccb71e8ea
Sarunpong Lapsuksathit <6432163521@student.chula.ac.th>
2024-03-11 00:45:34 +0700

    Refractor and componentize code

--- components/JobsPanel/JobsMenu.tsx
+++ components/JobsPanel/JobsMenu.tsx
@@ -3,18 +3,18 @@
 import React, { useState, useEffect } from "react";
 import JobsPanel from "./JobsPanel";
 import Link from "next/link";
-import { CloseOutlined, SortAscendingOutlined } from "@ant-design/icons";
 import JobCardType from "../../types/JobCardType";
 import fetchInitialData from "../../lib/Jobs/fetchInitialData";
 import { useSession } from "next-auth/react";
 import JobToggler from "./JobToggler";
+import Sorter from "./Sorter";
 
 type Props = {};
 
 const jobTypeList = ['งานปัจจุบัน', 'งานที่เสร็จสิ้น'];
 
 // export function
-const JobsMenu = () => {
+function JobsMenu(props: Props){
   const { data: session } = useSession();
   const [jobType, setJobType] = useState('งานปัจจุบัน');
   const [startDateSortOption, setStartDateSortOption] = useState("-");
@@ -47,9 +47,9 @@ const JobsMenu = () => {
 
   return (
     <>
+
       {/* Toggle between PendingJobsPanel and DoneJobsPanel based on the value of isPending. */}
       <JobToggler status={jobType} setStatus={setJobType} statusList={jobTypeList} />
-      
 
       {/* Sort button and Create Work button */}
       <section className="my-3">
@@ -58,17 +58,17 @@ const JobsMenu = () => {
           <div className="bg-transparent text-transparent py-2">|</div>
 
           {/* Sort button */}
-          <button
-            className="bg-slate-300 font-medium text-md rounded-md px-3 py-2 hover:shadow-md lg:hidden"
-            onClick={() => {
-              setIsOpeningSideBar(true);
-            }}
-          >
-            <div className="flex align-center">
-              <SortAscendingOutlined className="flex place-items-center" />
-              <p>จัดเรียง</p>
-            </div>
-          </button>
+          <Sorter isOpeningSideBar={isOpeningSideBar}
+                  setIsOpeningSideBar={setIsOpeningSideBar} 
+                  startDateSortOption={startDateSortOption} 
+                  setStartDateSortOption={setStartDateSortOption} 
+                  endDateSortOption={endDateSortOption} 
+                  setEndDateSortOption={setEndDateSortOption} 
+                  priceSortOption={priceSortOption} 
+                  setPriceSortOption={setPriceSortOption} 
+                  applicantsSortOption={applicantsSortOption} 
+                  setApplicantsSortOption={setApplicantsSortOption}
+            />
 
           {/* Create Work button */}
           <Link href={"/jobs/create"} key={"createJob"}>
@@ -81,6 +81,7 @@ const JobsMenu = () => {
         </div>
       </section>
 
+      {/* Show jobs */}
       <div className="lg:flex lg:flex-row lg:justify-between gap-2">
         {/* PendingJobsPanel and DoneJobsPanel */}
         {jobType === 'งานปัจจุบัน' ? (
@@ -101,206 +102,6 @@ const JobsMenu = () => {
             data={doneJobs}
             isPending={jobType === 'งานปัจจุบัน'}
           ></JobsPanel>
-        )}
-
-        {/* Sidebar for sorting for laptop */}
-        <aside className="hidden lg:flex lg:flex-col bg-slate-50 rounded-sm pt-7 pb-2 px-4 w-[200px] h-fit shadow-xl">
-          <div className="text-2xl font-semibold">จัดเรียง</div>
-          <div className="text-lg font-semibold mt-4 mb-2 text-slate-800">
-            ช่วงเวลา
-          </div>
-          <div className="flex flex-col gap-1 mb-2">
-            <p className="font-medium text-[16px] text-slate-800 mb-1">
-              วันที่เริ่มต้น
-            </p>
-            <select
-              value={startDateSortOption}
-              onChange={(e) => {
-                setStartDateSortOption(e.target.value);
-              }}
-              className="bg-slate-50 border border-slate-300 text-slate-800 text-[16px] rounded-lg focus:outline-none focus:ring-1 focus:ring-slate-500 focus:border-slate-500 w-full p-2"
-            >
-              <option value="">-</option>
-              <option value="asc">น้อยไปมาก</option>
-              <option value="desc">มากไปน้อย</option>
-            </select>
-          </div>
-          <div className="flex flex-col gap-1">
-            <p className="font-medium text-[16px] text-slate-800 mb-1">
-              วันที่สิ้นสุด
-            </p>
-            <select
-              value={endDateSortOption}
-              onChange={(e) => {
-                setEndDateSortOption(e.target.value);
-              }}
-              className="bg-slate-50 border border-slate-300 text-slate-800 text-[16px] rounded-lg focus:outline-none focus:ring-1 focus:ring-slate-500 focus:border-slate-500 w-full p-2"
-            >
-              <option value="">-</option>
-              <option value="asc">น้อยไปมาก</option>
-              <option value="desc">มากไปน้อย</option>
-            </select>
-          </div>
-          <div className="text-lg font-semibold mt-4 mb-2 text-slate-800">
-            ราคา
-          </div>
-          <div className="flex flex-col gap-1 mb-2">
-            <select
-              value={priceSortOption}
-              onChange={(e) => {
-                setPriceSortOption(e.target.value);
-              }}
-              className="bg-slate-50 border border-slate-300 text-slate-800 text-[16px] rounded-lg focus:outline-none focus:ring-1 focus:ring-slate-500 focus:border-slate-500 w-full p-2"
-            >
-              <option value="">-</option>
-              <option value="asc">น้อยไปมาก</option>
-              <option value="desc">มากไปน้อย</option>
-            </select>
-          </div>
-          <div className="text-lg font-semibold mt-4 mb-2 text-slate-800">
-            จำนวนผู้สมัคร
-          </div>
-          <div className="flex flex-col gap-1 mb-2">
-            <select
-              value={applicantsSortOption}
-              onChange={(e) => {
-                setApplicantsSortOption(e.target.value);
-              }}
-              className="bg-slate-50 border border-slate-300 text-slate-800 text-[16px] rounded-lg focus:outline-none focus:ring-1 focus:ring-slate-500 focus:border-slate-500 w-full p-2"
-            >
-              <option value="">-</option>
-              <option value="asc">น้อยไปมาก</option>
-              <option value="desc">มากไปน้อย</option>
-            </select>
-          </div>
-          <div className="mt-2">
-            <button
-              onClick={() => {
-                setStartDateSortOption("");
-                setEndDateSortOption("");
-                setPriceSortOption("");
-                setApplicantsSortOption("");
-              }}
-              className="w-full min-h-[40px] text-slate-700 text-[16px] rounded-md hover:bg-slate-200 focus:ring-2 focus:outline-none focus:ring-slate-300"
-            >
-              ล้าง
-            </button>
-          </div>
-        </aside>
-
-        {/* Overlay for sidebar for sorting for mobile */}
-        {isOpeningSideBar ? (
-          <>
-            <div className="z-10 bg-neutral-800 opacity-60 fixed top-0 right-0 left-0 bottom-0 lg:hidden"></div>
-            <aside className="fixed font-ibm z-20 bg-slate-50 text-slate-900 top-0 left-0 w-2/3 h-screen flex flex-col items-center pt-7 px-7 pb-40 md:pb-7 justify-between lg:hidden overflow-y-auto">
-              <div className="flex flex-col gap-8 justify-start w-full">
-                <div className="flex flex-row justify-between">
-                  <div className="text-3xl font-bold text-slate-800 mb-6">
-                    จัดเรียง
-                  </div>
-                  <CloseOutlined
-                    onClick={() => {
-                      setIsOpeningSideBar(false);
-                    }}
-                  />
-                </div>
-
-                <div className="text-xl font-bold text-slate-800 mt-2">
-                  ช่วงเวลา
-                </div>
-                <div className="flex flex-row justify-between gap-2">
-                  <div className="flex flex-col w-full">
-                    <p className="font-medium text-[16px] text-slate-800 mb-1">
-                      วันที่เริ่มต้น
-                    </p>
-                    <select
-                      value={startDateSortOption}
-                      onChange={(e) => {
-                        setStartDateSortOption(e.target.value);
-                      }}
-                      className="bg-slate-50 border border-slate-300 text-slate-800 text-[16px] rounded-lg focus:outline-none focus:ring-1 focus:ring-slate-500 focus:border-slate-500 w-full p-2"
-                    >
-                      <option value="">-</option>
-                      <option value="asc">น้อยไปมาก</option>
-                      <option value="desc">มากไปน้อย</option>
-                    </select>
-                  </div>
-                  <div className="flex flex-col w-full">
-                    <p className="font-medium text-[16px] text-slate-800 mb-1">
-                      วันที่สิ้นสุด
-                    </p>
-                    <select
-                      value={endDateSortOption}
-                      onChange={(e) => {
-                        setEndDateSortOption(e.target.value);
-                      }}
-                      className="bg-slate-50 border border-slate-300 text-slate-800 text-[16px] rounded-lg focus:outline-none focus:ring-1 focus:ring-slate-500 focus:border-slate-500 w-full p-2"
-                    >
-                      <option value="">-</option>
-                      <option value="asc">น้อยไปมาก</option>
-                      <option value="desc">มากไปน้อย</option>
-                    </select>
-                  </div>
-                </div>
-
-                <div className="text-xl font-bold text-slate-800 mt-2">
-                  ราคา
-                </div>
-                <div className="flex flex-col gap-2 mb-2">
-                  <select
-                    value={priceSortOption}
-                    onChange={(e) => {
-                      setPriceSortOption(e.target.value);
-                    }}
-                    className="bg-slate-50 border border-slate-300 text-slate-800 text-[16px] rounded-lg focus:outline-none focus:ring-1 focus:ring-slate-500 focus:border-slate-500 w-full p-2"
-                  >
-                    <option value="">-</option>
-                    <option value="asc">น้อยไปมาก</option>
-                    <option value="desc">มากไปน้อย</option>
-                  </select>
-                </div>
-                <div className="text-xl font-bold text-slate-800 mt-2">
-                  จำนวนผู้สมัคร
-                </div>
-                <div className="flex flex-col gap-1 mb-2">
-                  <select
-                    value={applicantsSortOption}
-                    onChange={(e) => {
-                      setApplicantsSortOption(e.target.value);
-                    }}
-                    className="bg-slate-50 border border-slate-300 text-slate-800 text-[16px] rounded-lg focus:outline-none focus:ring-1 focus:ring-slate-500 focus:border-slate-500 w-full p-2"
-                  >
-                    <option value="">-</option>
-                    <option value="asc">น้อยไปมาก</option>
-                    <option value="desc">มากไปน้อย</option>
-                  </select>
-                </div>
-              </div>
-              <div className="flex flex-row justify-between gap-2 w-full mt-8 md:mt-0">
-                <button
-                  onClick={() => {
-                    setStartDateSortOption("");
-                    setEndDateSortOption("");
-                    setPriceSortOption("");
-                    setApplicantsSortOption("");
-                  }}
-                  className="w-1/2 min-h-[40px] text-slate-700 text-[16px] rounded-md hover:bg-slate-200 focus:ring-2 focus:outline-none focus:ring-slate-300"
-                >
-                  ล้าง
-                </button>
-                <button
-                  className="w-1/2 min-h-[40px] text-white text-[16px] rounded-md bg-slate-700 hover:bg-slate-600 focus:ring-4 focus:outline-none focus:ring-slate-300"
-                  onClick={() => {
-                    setIsOpeningSideBar(false);
-                  }}
-                >
-                  จัดเรียง
-                </button>
-              </div>
-            </aside>
-          </>
-        ) : (
-          ""
         )}
       </div>
     </>
