fe496f029cc08c349c9ada6d915a7eb0b05da3c6
Surapee <suwan.srp@gmail.com>
2024-03-10 22:29:22 +0700

    fix: delete unused import line

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
 import { Inter, IBM_Plex_Sans_Thai } from "next/font/google";
 import "./globals.css";
-import Header from "@/components/header/Header";
-import Footer from "@/components/footer/Footer";
 import { cn } from "@/lib/utils";
 import AuthProvider from "@/providers/AuthProvider";
 import { Toaster } from "react-hot-toast";
