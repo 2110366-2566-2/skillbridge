d9001ff88d79937c162c64556a178f496538a669
Jai <jaijai211075@gmail.com>
2024-03-11 11:54:07 +0700

    fix: chat text color

--- components/jobStatus/dropDownObject/taskPaymentStatusBox.tsx
+++ components/jobStatus/dropDownObject/taskPaymentStatusBox.tsx
@@ -10,7 +10,9 @@ export default function TaskPaymentStatusBox({
         <div className="w-full flex justify-between mt-[10px] py-[5px]">
             <button className="h-[35px] bg-[#f8fafc] text-sm text-white rounded-md w-[100%] hover:opacity-80 active:opacity-60 text-black border border-[#334155] flex justify-center items-center">
                 <Image src={'/icons/chat.svg'} alt="chat" width={13} height={13} className="mr-[3px]" />
-                แชท
+                <p className="text-[#334155]">
+                    แชท
+                </p>
             </button>
         </div>
     )
