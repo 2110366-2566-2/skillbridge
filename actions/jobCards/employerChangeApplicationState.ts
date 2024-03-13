aefb4b169d5020af1d3304b397c31d035409135f
Fei-7 <supachokang@gmail.com>
2024-03-13 15:48:10 +0700

    feat : send email to student for every actions employer do to application

--- actions/jobCards/employerChangeApplicationState.ts
+++ actions/jobCards/employerChangeApplicationState.ts
@@ -4,6 +4,7 @@ import { prisma } from "../../lib/prisma";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/app/api/auth/[...nextauth]/auth";
 import { getApplication, getEmployerUserId, validateJobOwner } from "./utils";
+import { sendEmail } from "./sendEmail";
 
 /*
 Application states : 
@@ -42,6 +43,20 @@ async function pendingToAccepted(studentUserId: string, jobId: string) {
             status: ApplicationStatus.ACCEPTED
         }
     });
+
+    const job = await prisma.job.findUniqueOrThrow({
+        where: {
+            id: jobId
+        },
+        select: {
+            title: true
+        }
+    })
+
+    const subject = `ใบสมัครสำหรับงาน ${job.title} ของคุณถูกรับคัดเลือกแล้ว`;
+    const text = `ใบสมัครสำหรับงาน ${job.title} ของคุณถูกรับคัดเลือกแล้ว`;
+
+    sendEmail(studentUserId, subject, text);
 }
 
 async function pendingToRejected(studentUserId: string, jobId: string) {
@@ -68,6 +83,20 @@ async function pendingToRejected(studentUserId: string, jobId: string) {
             status: ApplicationStatus.REJECTED
         }
     });
+
+    const job = await prisma.job.findUniqueOrThrow({
+        where: {
+            id: jobId
+        },
+        select: {
+            title: true
+        }
+    })
+
+    const subject = `ใบสมัครสำหรับงาน ${job.title} ของคุณถูกไม่ผ่านการคัดเลือก`;
+    const text = `ใบสมัครสำหรับงาน ${job.title} ของคุณถูกไม่ผ่านการคัดเลือก`;
+
+    sendEmail(studentUserId, subject, text);
 }
  
 async function depositPendingToInProgress(studentUserId: string, jobId: string) {
@@ -95,6 +124,20 @@ async function depositPendingToInProgress(studentUserId: string, jobId: string) 
             status: ApplicationStatus.IN_PROGRESS
         }
     });
+
+    const job = await prisma.job.findUniqueOrThrow({
+        where: {
+            id: jobId
+        },
+        select: {
+            title: true
+        }
+    })
+
+    const subject = `ผู้จ้างได้จ่ายค่ามัจจำสำหรับงาน ${job.title} แล้ว`;
+    const text = `ผู้จ้างได้จ่ายค่ามัจจำสำหรับงาน ${job.title} แล้ว สามารถเริ่มทำงานได้`;
+
+    sendEmail(studentUserId, subject, text);
 }
  
 async function deliveredToInProgress(studentUserId: string, jobId: string) {
@@ -121,6 +164,20 @@ async function deliveredToInProgress(studentUserId: string, jobId: string) {
             status: ApplicationStatus.IN_PROGRESS
         }
     });
+
+    const job = await prisma.job.findUniqueOrThrow({
+        where: {
+            id: jobId
+        },
+        select: {
+            title: true
+        }
+    })
+
+    const subject = `ผู้จ้างปฏิเสธงาน ${job.title} ของคุณ`;
+    const text = `ผู้จ้างปฏิเสธงาน ${job.title} ของคุณ กรุณาปรับปรุงงานและส่งมอบงานใหม่`;
+
+    sendEmail(studentUserId, subject, text);
 }
 
 async function deliveredToCanceled(studentUserId: string, jobId: string) {
@@ -147,6 +204,20 @@ async function deliveredToCanceled(studentUserId: string, jobId: string) {
             status: ApplicationStatus.CANCELED
         }
     });
+
+    const job = await prisma.job.findUniqueOrThrow({
+        where: {
+            id: jobId
+        },
+        select: {
+            title: true
+        }
+    })
+
+    const subject = `ผู้จ้างยกเลิกงาน ${job.title} ของคุณ`;
+    const text = `ผู้จ้างยกเลิกงาน ${job.title} ของคุณ`;
+
+    sendEmail(studentUserId, subject, text);
 }
 
 async function deliveredToWagePaymentPending(studentUserId: string, jobId: string) {
@@ -173,6 +244,20 @@ async function deliveredToWagePaymentPending(studentUserId: string, jobId: strin
             status: ApplicationStatus.WAGE_PAYMENT_PENDING
         }
     });
+
+    const job = await prisma.job.findUniqueOrThrow({
+        where: {
+            id: jobId
+        },
+        select: {
+            title: true
+        }
+    })
+
+    const subject = `ผู้จ้างยอมรับการส่งมอบงาน ${job.title} ของคุณแล้ว`;
+    const text = `ผู้จ้างยอมรับการส่งมอบงาน ${job.title} ของคุณแล้ว กรุณารอผู้จ้างจ่ายค่าจ้างที่เหลือ`;
+
+    sendEmail(studentUserId, subject, text);
 }
 
 async function wagePaymentPendingToDone(studentUserId: string, jobId: string) {
@@ -199,6 +284,20 @@ async function wagePaymentPendingToDone(studentUserId: string, jobId: string) {
             status: ApplicationStatus.DONE
         }
     });
+
+    const job = await prisma.job.findUniqueOrThrow({
+        where: {
+            id: jobId
+        },
+        select: {
+            title: true
+        }
+    })
+
+    const subject = `ผู้จ้างจ่ายค่าจ้างสำหรับงาน ${job.title} ของคุณแล้ว`;
+    const text = `ผู้จ้างจ่ายค่าจ้างสำหรับงาน ${job.title} ของคุณแล้ว`;
+
+    sendEmail(studentUserId, subject, text);
 }
 
 export { pendingToAccepted, pendingToRejected, depositPendingToInProgress, deliveredToInProgress, deliveredToCanceled, deliveredToWagePaymentPending, wagePaymentPendingToDone };
