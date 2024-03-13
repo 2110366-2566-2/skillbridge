7ec81e3ece9373c1862e4fdb75c5960a12f1bcb5
SupanutT <supanut.tangsin@gmail.com>
2024-03-13 17:05:29 +0700

    feat : qrReader done

--- actions/payment/verifySlip.ts
+++ actions/payment/verifySlip.ts
@@ -1,20 +1,19 @@
 "use server"
 
-import getS3URL from "../../lib/S3/getS3URL";
+import getS3URL from "../S3/getS3URL";
 import qrReader from "../../lib/qrReader";
 
 async function verifySlip(fileName: string) {
-    console.log(process.env.NEXT_PUBLIC_BUCKET_REGION)
     const url = await getS3URL(fileName);
-    const response = await qrReader(url as string)
-    console.log(response)
+    const data = await qrReader(url as string)
+    console.log("data", data)
 }
 
 export default verifySlip;
 
-const main = async () => {
-    const fileName = '014071184526APP07376.jpg'
-    await verifySlip(fileName)
-};
+// const main = async () => {
+//     const fileName = '014071184526APP07376.jpg'
+//     await verifySlip(fileName)
+// };
 
-main();
+// main();
--- lib/qrReader.ts
+++ lib/qrReader.ts
@@ -1,15 +1,18 @@
 export default async function qrReader(url: string) {
     const encodedUrl = encodeURIComponent(url);
-    // http://api.qrserver.com/v1/read-qr-code/?fileurl=
     try {
         const res = await fetch(`http://api.qrserver.com/v1/read-qr-code/?fileurl=${encodedUrl}`, {
-            method: "GET"
+            method: "GET",
         })
-        console.log(encodedUrl)
         const response = await res.json();
-        // console.log(response)
-        return response
+        const data = response[0].symbol[0].data;
+
+        /* if the image has no qr code it will return with status 404 */
+        return {
+            message: data ? data : 'QR code not found.',
+            status: data ? 200 : 404
+        }
     } catch (error) {
-        throw new Error("Failed to read the QR code")
+        throw new Error("Failed to read the QR code.")
     }
 }
