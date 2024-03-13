f0c87047abece9661f5ef1498989a4d8479cafb4
SupanutT <supanut.tangsin@gmail.com>
2024-03-14 01:30:12 +0700

    feat : verify payment slip

--- actions/S3/getS3URL.ts
+++ actions/S3/getS3URL.ts
@@ -6,16 +6,12 @@ import s3 from "../../lib/bucket";
 
 const getS3URL = async (fileName: string) => {
   try {
-    console.log("BUCKET_NAME", process.env.BUCKET_NAME)
     const getObjectParams = {
       Bucket: process.env.BUCKET_NAME,
       Key: fileName,
     };
-    console.log(1)
     const command = new GetObjectCommand(getObjectParams);
-    console.log(2)
     const url = await getSignedUrl(s3, command, { expiresIn: 3600 });
-    console.log(3)
     return url;
   } catch (error) {
     console.log(error);
@@ -24,11 +20,3 @@ const getS3URL = async (fileName: string) => {
 };
 
 export default getS3URL;
-
-// const main = async () => {
-//   const fileName = "03519fc459139620652729caf68ba68d8bb17c5208ba055ae59b5516905c5a79"
-//   const result = await getS3URL(fileName)
-//   console.log(result)
-// };
-
-// main();
--- actions/payment/verifySlip.ts
+++ actions/payment/verifySlip.ts
@@ -2,11 +2,104 @@
 
 import getS3URL from "../S3/getS3URL";
 import qrReader from "../../lib/qrReader";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/auth";
+import { prisma } from "../../lib/prisma";
 
-async function verifySlip(fileName: string) {
-    const url = await getS3URL(fileName);
-    const data = await qrReader(url as string)
-    console.log("data", data)
+async function verifySlip(transactionId: string, fileName: string) {
+    try {
+        /* Check for authorization as this service only available for employer */
+        const session: any = await getServerSession(authOptions);
+        const userId = session?.user.id;
+        const employer = await prisma.employer.findFirst({
+            where: { userId: userId },
+            select: { userId: true },
+        });
+
+        if (!session || !employer) {
+            return {
+                success: false,
+                message: "Not Authenticated",
+                status: 401,
+            };
+        }
+
+        /* Get signed url and extract the data from qrcode */
+        const url = await getS3URL(fileName);
+        const QRdata = await qrReader(url as string)
+
+        /* qrcode not found */
+        if (QRdata.status === 404) {
+            return {
+                success: false,
+                message: "QR code not found",
+                status: 404
+            }
+        }
+
+        /* qrcode found */
+        if (QRdata.status === 200) {
+            const res = await fetch(`https://api.slipok.com/api/line/apikey/${process.env.SLIPOK_BRANCH_ID}`, {
+                method: "POST",
+                headers: {
+                    'x-authorization': process.env.SLIPOK_API_KEY as string,
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    data: QRdata.message
+                })
+            })
+
+            const response = await res.json();
+            const { data } = response;
+
+            if (response.success && data.success) {
+                /* Create new transactionDetail object */
+                const transactionDetail = await prisma.transactionDetail.create({
+                    data: {
+                        transactionId: transactionId,
+                        transRef: data.transRef,
+                        sendingBank: data.sendingBank,
+                        transTimestamp: new Date(data.transTimestamp),
+                        senderTHName: data.sender.displayName,
+                        senderENName: data.sender.name,
+                        senderAccountType: data.sender.account.type,
+                        senderAccountValue: data.sender.account.value,
+                        receiverTHName: data.receiver.displayName,
+                        receiverENName: data.receiver.name,
+                        amount: data.amount,
+                        qrcodeData: data.qrcodeData
+                    }
+                })
+                return {
+                    success: true,
+                    message: "The payment slip is successfully verified",
+                    status: 201
+                }
+            } else {
+                return {
+                    success: false,
+                    message: "Cannot read the provided payment slip",
+                    status: 400
+                }
+            }
+
+        }
+
+    } catch (error) {
+        if (error instanceof Error) {
+            return {
+                message: error.message,
+                status: 500
+            };
+        } else {
+            return {
+                message: "Internal Server Error",
+                status: 500
+            }
+        }
+    }
+
 }
 
 export default verifySlip;
