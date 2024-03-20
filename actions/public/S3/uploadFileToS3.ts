e6e4472dde263dd735c093b2543c8970dc2bb092
naratip <naratip.18257@gmail.com>
2024-03-20 15:41:21 +0700

    adding folder shcema for uploadFiles and Testing

--- actions/public/S3/uploadFileToS3.ts
+++ actions/public/S3/uploadFileToS3.ts
@@ -1,17 +1,21 @@
 "use server";
-
+// require("dotenv").config(); // For Testing
 import { PutObjectCommand, GetObjectCommand } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
-import s3 from "@/lib/bucket";
+import s3 from "../../../lib/bucket";
 import crypto from "crypto";
 const generateFileName = (bytes = 32) =>
   crypto.randomBytes(bytes).toString("hex");
+
+interface Error {
+  message: string;
+}
 
 const uploadFileToS3 = async (
   buffer: Uint8Array,
   type: string,
   size: number,
-  path: string,
+  path: string
 ) => {
   const validPath = ["jobFiles", "applicationFiles", "transactionFiles"];
 
@@ -21,11 +25,11 @@ const uploadFileToS3 = async (
     };
   }
   const fileName = generateFileName();
-  // const URL = `${process.env.BUCKET_NAME}.s3.${process.env.BUCKET_REGION}.amazonaws.com/${fileName}`;
+  const URL = `${path}/${fileName}`;
 
   const putObjectCommand = new PutObjectCommand({
     Bucket: process.env.BUCKET_NAME,
-    Key: fileName,
+    Key: URL,
     Body: buffer,
     ContentType: type,
     ContentLength: size,
@@ -33,11 +37,35 @@ const uploadFileToS3 = async (
 
   try {
     const response = await s3.send(putObjectCommand);
-    return fileName;
+    // console.log(response);
+    return URL;
   } catch (error) {
     console.log(error);
-    throw { message: "Upload Failed" };
+    throw { message: "Upload Failed" } as Error;
   }
 };
 
 export default uploadFileToS3;
+
+//TEST
+// import getS3Url from "./getS3URL";
+// import { loadEnvConfig } from "@next/env";
+// import { z } from "zod";
+// const main = async () => {
+//   loadEnvConfig("../../../.env.local");
+//   const file: File = new File(["1234"], "filename", {
+//     type: "application/pdf",
+//   });
+//   const type: string = file.type;
+//   const size: number = file.size;
+//   const path = "jobFiles";
+//   const buffer: Uint8Array = new Uint8Array(await file.arrayBuffer());
+//   const result: string | Error = await uploadFileToS3(buffer, type, size, path);
+//   console.log(result);
+//   if (z.string().safeParse(result)) {
+//     const url = await getS3Url(result as string);
+//     console.log(url);
+//   }
+// };
+
+// main();
