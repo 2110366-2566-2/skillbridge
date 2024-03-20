395f3bdb71daa21faf88ac9895b122f7cc712132
Jai <jaijai211075@gmail.com>
2024-03-21 01:59:59 +0700

    fix: profile info

--- app/(withNavigation)/profile/[studentId]/page.tsx
+++ app/(withNavigation)/profile/[studentId]/page.tsx
@@ -8,7 +8,13 @@ export default async function ProfilePage({
 
     return (
         <div className="w-full h-full">
-            <ProfileInfo profileImageURL="" studentName="ไอไจ๋มันหล่อ จังฟร่ะ" averageScore={4.9} />
+            <ProfileInfo
+                profileImageURL=""
+                studentName="ไอไจ๋มันหล่อ จังฟร่ะ"
+                averageScore={4.9}
+                portfolioURL="/"
+                studentDetail="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+            />
         </div>
     );
 }
--- components/profile/ProfileInfo.tsx
+++ components/profile/ProfileInfo.tsx
@@ -1,15 +1,23 @@
+'use client'
 import Image from "next/image";
 import RatingScore from "./subProfile/RatingScore";
+import { useRouter } from "next/navigation";
 
 export default function ProfileInfo({
     profileImageURL,
     studentName,
     averageScore,
+    portfolioURL,
+    studentDetail,
 }: {
     profileImageURL: string,
     studentName: string,
-    averageScore: number
+    averageScore: number,
+    portfolioURL: string
+    studentDetail: string,
 }) {
+
+    const router = useRouter();
 
     return (
         <div className="p-[20px] w-full border border-1 border-[#cbd5e1] rounded-md">
@@ -36,8 +44,20 @@ export default function ProfileInfo({
 
                 </div>
             </div>
-            <div>
 
+            <div className="w-full px-[16px] flex items-center py-[8px] bg-[#cbd5e1] rounded-lg hover:underline hover:underline-offset-2 cursor-pointer mb-[10px]"
+                onClick={() => router.push(portfolioURL)}
+            >
+                <Image src={'/icons/file.svg'} width={15} height={15} alt="file" className="mr-[15px]" />
+                <p className="text-[16px] font-medium text-slate-600 text-center">
+                    แฟ้มสะสมผลงาน
+                </p>
+            </div>
+
+            <div className="py-[15px] border-y border-[#cbd5e1] mb-[10px]">
+                <p className="line-clamp-5 text-[13px] text-[#64748B]">
+                    {studentDetail}
+                </p>
             </div>
         </div>
     );
