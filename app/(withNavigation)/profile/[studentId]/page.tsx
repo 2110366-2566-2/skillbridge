3be5343a2521a55066ff5438d8c45934999fcdf4
LittleNa1000 <naphanccnp@gmail.com>
2024-03-24 00:49:17 +0700

    feat: /profile open resume url

--- app/(withNavigation)/profile/[studentId]/page.tsx
+++ app/(withNavigation)/profile/[studentId]/page.tsx
@@ -3,17 +3,25 @@ import JobsHistoryList from "@/components/profile/JobsHistoryList"
 import getJobsByStudentId from "@/actions/application/getJobsByStudentId"
 import { getStudentInfoById } from "@/actions/public/getUserInfo"
 import { getReviewsByStudentId } from "@/actions/reviews/getReviews"
+import getS3URL from "@/actions/public/S3/getS3URL"
+import { getServerSession } from "next-auth"
+import { authOptions } from "@/app/api/auth/[...nextauth]/auth"
 
 export default async function ProfilePage({ params }: { params: { studentId: string } }) {
-  const [allJobsHistory, user, reviews] = await Promise.all([
+  const [allJobsHistory, user, reviews, session] = await Promise.all([
     getJobsByStudentId(params.studentId),
     getStudentInfoById(params.studentId),
     getReviewsByStudentId(params.studentId),
+    getServerSession(authOptions),
   ])
+
+  const portfolioURL = user?.student?.resumeName ? await getS3URL(user?.student?.resumeName) : null
+
   const averageScore = reviews
     ? reviews.reduce((acc, review) => acc + review.stars, 0) / reviews.length
     : 0
   const success = allJobsHistory.reduce((acc, job) => (job.status === "DONE" ? acc + 1 : acc), 0)
+  const successRate = Math.round((success * 100) / allJobsHistory.length)
 
   return (
     <div className="w-full md:flex md:justify-center md:px-[4px] md:py-[16px]">
@@ -23,10 +31,11 @@ export default async function ProfilePage({ params }: { params: { studentId: str
           profileImageURL={user?.profileImageUrl || ""}
           studentName={`${user?.salutation} ${user?.firstname} ${user?.lastname}` || ""}
           averageScore={averageScore || 0}
-          portfolioURL={user?.student?.resumeName || ""}
+          portfolioURL={portfolioURL?.data || ""}
           studentDetail={user?.student?.description || ""}
           workingNumber={allJobsHistory.length}
-          workingComplete={Math.round((success * 100) / allJobsHistory.length)}
+          workingComplete={isNaN(successRate) ? 0 : successRate}
+          session={session}
         />
       </div>
       <JobsHistoryList allJobsHistory={allJobsHistory} />
