d008a96a9d0421f7be3fde29ebcce8938be568e0
Jai <jaijai211075@gmail.com>
2024-03-24 14:39:50 +0700

    Revert "feat: finish job history and profile information"
    This reverts commit 36968db3670ad1e9de33c1d15e1ffa738b513691.

--- components/profile/ProfileInfo.tsx
+++ components/profile/ProfileInfo.tsx
@@ -30,16 +30,17 @@ export default function ProfileInfo({
   return (
     <div className="p-[20px] border border-1 border-[#cbd5e1] rounded-md md:px-[25px] md:py-[30px]">
       <div className="flex items-center mb-[15px] md:mb-[25px]">
-        <div className="mr-[15px] md:mr-[20px] w-[94px] h-[94px] lg:w-[130px] lg:h-[130px] rounded-full border border-[#778499] overflow-hidden">
+        <div className="mr-[15px] md:mr-[20px]">
           <Image
             src={profileImageURL ? profileImageURL : "/images/defaultProfile.svg"}
             width={0}
             height={0}
             alt="profile"
-            className="w-[110px] h-[110px] lg:w-[146px] lg:h-[146px] object-cover"
+            className="w-[94px] h-[94px] lg:w-[130px] lg:h-[130px]"
           />
         </div>
         <div className="flex flex-col">
+
           <p className="font-bold text-[21px] text-[#334155] line-clamp-1 lg:text-[28px]">
             {studentName}
           </p>
@@ -52,59 +53,65 @@ export default function ProfileInfo({
         </div>
       </div>
 
-      {portfolioURL && (
-        <Link href={portfolioURL}>
-          <div className="w-full px-[16px] flex items-center pb-[8px] pt-[10px] bg-[#cbd5e1] rounded-lg hover:underline hover:underline-offset-2 cursor-pointer mb-[15px] md:mb-[25px]">
-            <Image
-              src={"/icons/file.svg"}
-              width={0}
-              height={0}
-              alt="file"
-              className="w-[15px] h-[15px] md:w-[20px] md:h-[20px] mr-[15px]"
-            />
-            <p className="text-[16px] font-medium text-slate-600 text-center md:text-[20px]">
-              แฟ้มสะสมผลงาน
-            </p>
-          </div>
-        </Link>
-      )}
-
-      {studentDetail && (
-        <div className="pt-[15px] border-t border-[#cbd5e1] mb-[15px]">
-          <p className="line-clamp-5 text-[13px] text-[#64748B] md:text-[18px]">{studentDetail}</p>
+      {
+        portfolioURL &&
+        <div className="w-full px-[16px] flex items-center pb-[8px] pt-[10px] bg-[#cbd5e1] rounded-lg hover:underline hover:underline-offset-2 cursor-pointer mb-[15px] md:mb-[25px]"
+          onClick={() => router.push(portfolioURL)}
+        >
+          <Image src={'/icons/file.svg'} width={0} height={0} alt="file" className="w-[15px] h-[15px] md:w-[20px] md:h-[20px] mr-[15px]" />
+          <p className="text-[16px] font-medium text-slate-600 text-center md:text-[20px]">
+            แฟ้มสะสมผลงาน
+          </p>
         </div>
-      )}
+      }
+
+      {
+        studentDetail &&
+        <div className="pt-[15px] border-t border-[#cbd5e1] mb-[15px]">
+          <p className="line-clamp-5 text-[13px] text-[#64748B] md:text-[18px]">
+            {studentDetail}
+          </p>
+        </div>
+      }
 
       <div className="flex items-center justify-center border-t border-[#cbd5e1] pt-[15px] md:pt-[25px]">
         <div className="flex items-center mr-[35px]">
-          <Image src={"/icons/bag.svg"} width={45} height={42} alt="bag" className="mr-[10px]" />
+          <Image src={'/icons/bag.svg'} width={45} height={42} alt="bag" className="mr-[10px]" />
           <div className="flex flex-col pt-[5px]">
-            <p className="text-[12px] text-[#475569]">รับงานแล้ว</p>
-            <p className="text-[16px] text-[#475569] font-bold">{workingNumber} คน</p>
+            <p className="text-[12px] text-[#475569]">
+              รับงานแล้ว
+            </p>
+            <p className="text-[16px] text-[#475569] font-bold">
+              {workingNumber} คน
+            </p>
           </div>
         </div>
         <div className="flex items-center">
-          <Image
-            src={"/icons/checkHand.svg"}
-            width={69}
-            height={42}
-            alt="check hand"
-            className="mr-[10px]"
-          />
+          <Image src={'/icons/checkHand.svg'} width={69} height={42} alt="check hand" className="mr-[10px]" />
           <div className="flex flex-col pt-[5px] text-center">
-            <p className="text-[12px] text-[#475569]">อัตราสำเร็จ</p>
-            <p className="text-[16px] text-[#475569] font-bold">{workingComplete} %</p>
+            <p className="text-[12px] text-[#475569]">
+              อัตราสำเร็จ
+            </p>
+            <p className="text-[16px] text-[#475569] font-bold">
+              {workingComplete} %
+            </p>
           </div>
         </div>
       </div>
 
-      {isEditAble && (
-        <div
-          className="w-full h-[40px] flex items-center justify-center rounded-[6px] border border-1 border-slate-300 cursor-pointer mt-[15px] md:mt-[25px] md:h-[50px]
+      {
+        true &&
+        <div className="w-full h-[40px] flex items-center justify-center rounded-[6px] border border-1 border-slate-300 cursor-pointer mt-[15px] md:mt-[25px] md:h-[50px]
                 hover:underline hover:underline-offset-2">
-          <p className="text-[14px] font-medium text-slate-600 md:text-[18px]">แก้ไขโปรไฟล์</p>
+          <p className="text-[14px] font-medium text-slate-600 md:text-[18px]">
+            แก้ไขโปรไฟล์
+          </p>
         </div>
-      )}
+      }
+
     </div>
+  )
+}
+    </div >
   )
 }
--- components/profile/subProfile/JobHistory.tsx
+++ components/profile/subProfile/JobHistory.tsx
@@ -12,22 +12,22 @@ export default function JobHistory({
 
 
     return (
-        <div className="mt-[13px] w-full flex pb-[13px] border-b border-[#cbd5e1] md:mt-[15px] md:pb-[15px]">
-            <div className="w-full flex flex-col mr-[20px] ">
+        <div className="mt-[13px] w-full max-w-[600px] flex pb-[13px] border-b border-[#cbd5e1]">
+            <div className="w-full flex flex-col mr-[20px]">
                 <p className="font-bold text-[18px] text-[#334155] line-clamp-1">
                     {jobTitle}
                 </p>
                 <div className="flex w-full">
-                    <p className="text-[14px] text-[#64748b] font-medium mr-[10px] lg:text-[16px]">
+                    <p className="text-[14px] text-[#64748b] font-medium mr-[10px]">
                         {jobPeriod}
                     </p>
-                    <div className="text-[14px] text-[#334155] text-center bg-[#E8E2F0] py-[1px] px-[6px] rounded-[4px] ">
+                    <div className="text-[14px] text-[#334155] text-center bg-[#E8E2F0] py-[1px] px-[6px] rounded-[4px]">
                         {jobTag}
                     </div>
                 </div>
             </div>
             <div className="flex items-center ">
-                <div className="rounded-full border border-[#E2E8F0] bg-white p-[8px] hover:opacity-80 active:opacity-60 cursor-pointer">
+                <div className="rounded-full border border-[#E2E8F0] bg-white p-[8px]">
                     <Image src={'/icons/right.svg'} width={20} height={20} alt="right" />
                 </div>
             </div>
