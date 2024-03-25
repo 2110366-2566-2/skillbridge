675e8d544c2fbb134fc4e3bc589116d62ae226ba
Awat Singdamrong <a.lubing@yahoo.com>
2024-03-25 19:23:57 +0700

    feat: ChatRoomHeader detail

--- components/chat/chatRoom/ChatRoom.tsx
+++ components/chat/chatRoom/ChatRoom.tsx
@@ -1,16 +1,19 @@
 import ChatRoomHeader from "./ChatRoomHeader"
 import ChatMessageList from "./ChatMessageList"
 import ChatInput from "./ChatInput"
+import { getChatRoomInfo } from "@/actions/chat/getChatRoomInfo"
 
 type Props = {
     isStudent: boolean,
     chatroomId: string,
 }
 
-export default function ChatRoom({ isStudent, chatroomId }: Props) {
+export default async function ChatRoom({ isStudent, chatroomId }: Props) {
+    const chatRoomInfo = await getChatRoomInfo(chatroomId)
+
     return (
         <div className="h-[100dvh] w-full flex flex-col bg-neutral-100 border border-[#CBD5E1] lg:h-[80vh]">
-            <ChatRoomHeader isStudent={isStudent} chatroomId={chatroomId} />
+            <ChatRoomHeader isStudent={isStudent} chatRoomInfo={chatRoomInfo} />
             <ChatMessageList isStudent={isStudent} chatroomId={chatroomId} />
             <ChatInput isStudent={isStudent} chatroomId={chatroomId} />
         </div>
--- components/chat/chatRoom/ChatRoomHeader.tsx
+++ components/chat/chatRoom/ChatRoomHeader.tsx
@@ -1,15 +1,70 @@
 "use client"
 
+import { ChatRoomInfo } from "@/actions/chat/getChatRoomInfo"
+import Image from "next/image"
+import downArrowDark from "@/public/icons/downArrowDark.svg";
+import Link from "next/link";
+
 type Props = {
     isStudent: boolean,
-    chatroomId: string,
+    chatRoomInfo: ChatRoomInfo,
 }
 
-export default function ChatRoomHeader({ isStudent, chatroomId }: Props) {
+export default function ChatRoomHeader({ isStudent, chatRoomInfo }: Props) {
+    const title = isStudent ? chatRoomInfo.job.title : `${chatRoomInfo.student.firstname || ""} ${chatRoomInfo.student.middlename || ""} ${chatRoomInfo.student.lastname || ""}`
+    const subtitle_1 = isStudent ? `${chatRoomInfo.employer.firstname || ""} ${chatRoomInfo.employer.middlename || ""} ${chatRoomInfo.employer.lastname || ""}` : ""
+    const subtitle_2 = isStudent ? `, ${chatRoomInfo.employer.position || ""}${chatRoomInfo.employer.position ? ", " : ""} ${chatRoomInfo.employer.position}` : chatRoomInfo.job.title
 
     return (
-        <div className="h-[90px] w-full flex flex-row bg-slate-50 border-b border-[#CBD5E1] lg:h-[94px]">
-            รอนอร์ท
+        <div className="h-[90px] w-full flex flex-row items-center bg-slate-50 border-b border-[#CBD5E1] px-3 lg:h-[94px]">
+            <Link
+                className="mr-3 lg:hidden"
+                href={"/chat"}
+            >
+                <Image
+                    className={`rotate-90`}
+                    src={downArrowDark}
+                    alt="arrow"
+                    width={24}
+                    height={24}
+                />
+            </Link>
+            <div className="flex flex-col justify-between gap-1">
+                <div className="font-medium text-[20px] text-slate-800 truncate line-clamp-1">
+                    {title}
+                </div>
+                <div className="flex flex-row text-[14px] text-slate-800 truncate line-clamp-1">
+                    <div className="font-medium">
+                        {subtitle_1}
+                    </div>
+                    <div>
+                        {subtitle_2}
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
+
+// export interface ChatRoomInfo {
+//     student: {
+//       id: string;
+//       salutation: string;
+//       firstname: string;
+//       middlename: string | null;
+//       lastname: string;
+//     };
+//     employer: {
+//       id: string;
+//       salutation: string;
+//       firstname: string;
+//       middlename: string | null;
+//       lastname: string;
+//       position: string;
+//       organization: string;
+//     };
+//     job: {
+//       id: string;
+//       title: string;
+//     };
+//   };
