91baea95fc41f0829f73887b9f63f1cd7aed82d2
Awat Singdamrong <a.lubing@yahoo.com>
2024-03-26 20:32:11 +0700

    fix: Hide log!

--- components/chat/chatRoom/ChatRoom.tsx
+++ components/chat/chatRoom/ChatRoom.tsx
@@ -64,7 +64,7 @@ export default function ChatRoom({ isStudent, chatroomId, senderId }: Props) {
                 "user-id": senderId!
             }
         });
-        console.log("connecting socket");
+        // console.log("connecting socket");
         curChatRoomId = chatroomId;
 
         firstLoad = false;
