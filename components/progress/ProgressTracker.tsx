763b35052ad5555b1e9feec92d4a4e6010798dec
Sarunpong Lapsuksathit <6432163521@student.chula.ac.th>
2024-03-26 15:37:43 +0700

    Add not found componentn

--- components/progress/ProgressTracker.tsx
+++ components/progress/ProgressTracker.tsx
@@ -1,7 +1,11 @@
 import { getEmploymentTracking } from "@/actions/employmentHistoryDetail/employmentHistoryDetail";
 import { EmploymentTrack } from "@/types/employmentTrackType";
 import React from "react";
-import ProgressIcon, { getIconColor, getThaiProgressText } from "./ProgressIcon";
+import ProgressIcon, {
+    getIconColor,
+    getThaiProgressText,
+} from "./ProgressIcon";
+import SearchNotFound from "../searchJob/SearchNotFound";
 
 type Props = {
     jobId: string;
@@ -64,63 +68,81 @@ function condenseBeforeAfterStates(data: Array<EmploymentTrack>) {
         return {
             status: `${item.status.before || ""}-${item.status.after}`,
             date: item.date,
-        }
-    })
+        };
+    });
 }
 
 async function ProgressTracker({ jobId, studentId }: Props) {
     const data: Array<EmploymentTrack> =
         (await getEmploymentTracking(jobId, studentId)).data || [];
     const condensedData = condenseBeforeAfterStates(data);
-    return condensedData.map((item, index) => {
-        if (index === 0) {
-            return (
-                <main key={index} className="flex flex-col min-w-[300px] xl:min-w-[400px]">
-                    <section className="flex flex-row justify-between items-center">
-                        <div className="flex flex-row items-center gap-4">
-                            <ProgressIcon status={item.status} />
-                            <p className="font-semibold text-md">
-                                {getThaiProgressText(item.status)}
+    if (condensedData.length === 0) {
+        return (
+            <div className="flex justify-center items-center">
+                <SearchNotFound text="ไม่พบประวัติ" />
+            </div>
+        );
+    } else {
+        return condensedData.map((item, index) => {
+            if (index === 0) {
+                return (
+                    <main
+                        key={index}
+                        className="flex flex-col min-w-[300px] xl:min-w-[400px]"
+                    >
+                        <section className="flex flex-row justify-between items-center">
+                            <div className="flex flex-row items-center gap-4">
+                                <ProgressIcon status={item.status} />
+                                <p className="font-semibold text-md">
+                                    {getThaiProgressText(item.status)}
+                                </p>
+                            </div>
+                            <p className="font-light text-md">
+                                {item.date.toLocaleDateString("en-GB")}
                             </p>
-                        </div>
-                        <p className="font-light text-md">{item.date.toLocaleDateString("en-GB")}</p>
-                    </section>
-                </main>
-            );
-        } else {
-            const color = getIconColor(item.status);
-            const prevColor = getIconColor(condensedData[index - 1].status);
-            return (
-                <main key={index} className="flex flex-col min-w-[300px] xl:min-w-[400px]">
-                    {/* vertical line */}
-                    <div
-                        className={`h-[45px] w-[3px] ml-[21px] border-slate-600 bg-gradient-to-t ${
-                            color === "red-400"
-                                ? "from-red-400"
-                                : color === "green-600"
-                                ? "from-green-600"
-                                : "from-slate-600"
-                        } ${
-                            prevColor === "red-400"
-                                ? "to-red-400"
-                                : prevColor === "green-600"
-                                ? "to-green-600"
-                                : "to-slate-600"
-                        }`}
-                    ></div>
-                    <section className="flex flex-row justify-between items-center">
-                        <div className="flex flex-row items-center gap-4">
-                            <ProgressIcon status={item.status} />
-                            <p className="font-semibold text-md">
-                                {getThaiProgressText(item.status)}
+                        </section>
+                    </main>
+                );
+            } else {
+                const color = getIconColor(item.status);
+                const prevColor = getIconColor(condensedData[index - 1].status);
+                return (
+                    <main
+                        key={index}
+                        className="flex flex-col min-w-[300px] xl:min-w-[400px]"
+                    >
+                        {/* vertical line */}
+                        <div
+                            className={`h-[45px] w-[3px] ml-[21px] border-slate-600 bg-gradient-to-t ${
+                                color === "red-400"
+                                    ? "from-red-400"
+                                    : color === "green-600"
+                                    ? "from-green-600"
+                                    : "from-slate-600"
+                            } ${
+                                prevColor === "red-400"
+                                    ? "to-red-400"
+                                    : prevColor === "green-600"
+                                    ? "to-green-600"
+                                    : "to-slate-600"
+                            }`}
+                        ></div>
+                        <section className="flex flex-row justify-between items-center">
+                            <div className="flex flex-row items-center gap-4">
+                                <ProgressIcon status={item.status} />
+                                <p className="font-semibold text-md">
+                                    {getThaiProgressText(item.status)}
+                                </p>
+                            </div>
+                            <p className="font-light text-md">
+                                {item.date.toLocaleDateString("en-GB")}
                             </p>
-                        </div>
-                        <p className="font-light text-md">{item.date.toLocaleDateString("en-GB")}</p>
-                    </section>
-                </main>
-            );
-        }
-    });
+                        </section>
+                    </main>
+                );
+            }
+        });
+    }
 }
 
 export default ProgressTracker;
