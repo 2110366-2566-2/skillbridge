36bac66db5059cae8fa8fbcb471a784b5bae397f
Sarunpong Lapsuksathit <6432163521@student.chula.ac.th>
2024-03-26 22:46:17 +0700

    Debug

--- components/jobManage/JobManagementPanel.tsx
+++ components/jobManage/JobManagementPanel.tsx
@@ -5,7 +5,7 @@ import { Student } from "@/types/StudentType";
 import React, { useEffect, useState } from "react";
 import SearchNotFound from "../searchJob/SearchNotFound";
 import { convertStateNameToThai } from "@/lib/Jobs/adapter";
-import StudentOfferCard from "../jobs/employerJobs/StudentOfferCard";
+import StudentOfferCard from "../jobs/employerJobs/employerCallToAction/StudentOfferCard";
 
 type Props = {
     jobId: string;
