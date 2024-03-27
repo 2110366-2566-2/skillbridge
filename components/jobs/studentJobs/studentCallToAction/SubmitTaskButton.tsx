14559251661acd5b58fecf2479e9233a881638e8
Sarunpong Lapsuksathit <6432163521@student.chula.ac.th>
2024-03-27 20:50:27 +0700

    Debug minibug

--- components/jobs/studentJobs/studentCallToAction/SubmitTaskButton.tsx
+++ components/jobs/studentJobs/studentCallToAction/SubmitTaskButton.tsx
@@ -6,8 +6,10 @@ import { useRouter } from "next/router";
 export default function SubmitTaskButton({ jobId }: { jobId: string }) {
   return (
       <div className="flex flex-row justify-between w-full gap-1">
-          <button className="py-2 px-3 w-full bg-slate-50 text-sm text-slate-900 border border-slate-700 rounded-md hover:shadow-md hover:border-slate-800 active:border-slate-900 transition duration-200 ease-in-out"
-          onClick={() => alert("go to chat")}>
+          <button
+              className="flex flex-row justify-center items-center py-2 px-3 w-full bg-slate-50 text-sm text-slate-900 border border-slate-700 rounded-md hover:shadow-md hover:border-slate-800 active:border-slate-900 transition duration-200 ease-in-out"
+              onClick={() => alert("go to chat")}
+          >
               <Image
                   src={"/icons/chat.svg"}
                   alt="chat"
