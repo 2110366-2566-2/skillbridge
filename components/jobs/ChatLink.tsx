c71d4725f49d173ee12dd9ebe768837e17f75b62
Sarunpong Lapsuksathit <6432163521@student.chula.ac.th>
2024-03-28 00:27:17 +0700

    reduce useeffect to 1

--- components/jobs/ChatLink.tsx
+++ components/jobs/ChatLink.tsx
@@ -20,35 +20,36 @@ function ChatLink({
     const [chatroomId, setChatroomId] = useState<string>("");
     const userId = useSession().data?.user?.id ?? "";
 
-    if (studentId === "") {
-        useEffect(() => {
+    useEffect(() => {
+        if (studentId === "") {
             const getChatroom = async () => {
-                console.log(`userId: ${userId}, employerId: ${employerId}, jobId: ${jobId}`)
+                console.log(
+                    `userId: ${userId}, employerId: ${employerId}, jobId: ${jobId}`
+                );
                 const chatroom = await getChatroomId(userId, employerId, jobId);
                 setChatroomId(chatroom);
             };
             getChatroom();
-        }, []);
-    } else if (employerId === "") {
-        useEffect(() => {
+        } else if (employerId === "") {
             const getChatroom = async () => {
                 const chatroom = await getChatroomId(studentId, userId, jobId);
                 setChatroomId(chatroom);
             };
             getChatroom();
-        }, []);
-    }
+        }
+    }, []);
+
     return (
-            <Link className={className} href={`/chat/${chatroomId}`}>
-                <Image
-                    src={"/icons/chat.svg"}
-                    alt="chat"
-                    width={13}
-                    height={13}
-                    className="mr-[3px]"
-                />
-                <p className="text-[#334155]">แชท</p>
-            </Link>
+        <Link className={className} href={`/chat/${chatroomId}`}>
+            <Image
+                src={"/icons/chat.svg"}
+                alt="chat"
+                width={13}
+                height={13}
+                className="mr-[3px]"
+            />
+            <p className="text-[#334155]">แชท</p>
+        </Link>
     );
 }
 
