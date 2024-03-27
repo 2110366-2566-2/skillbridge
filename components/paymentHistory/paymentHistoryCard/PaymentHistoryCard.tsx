a2ed9a233b5b3241132d2264b39bcf4453e287e3
Surapee <suwan.srp@gmail.com>
2024-03-28 04:46:19 +0700

    fix: rounded 2 decimal place to payment and payment-history page

--- components/paymentHistory/paymentHistoryCard/PaymentHistoryCard.tsx
+++ components/paymentHistory/paymentHistoryCard/PaymentHistoryCard.tsx
@@ -63,7 +63,7 @@ export default function PaymentHistoryCard(props: Transaction) {
     const studentSign = isStudent ? "+" : "-";
     let netAmount = amount;
     if (isStudent) netAmount = getReal(netAmount);
-    const result = `${studentSign} ${currencySymbol}${netAmount.toLocaleString()}`;
+    const result = `${studentSign} ${currencySymbol}${netAmount.toFixed(2)}`;
     return result;
   }
 
@@ -130,17 +130,17 @@ export default function PaymentHistoryCard(props: Transaction) {
           <p>
             {props.isDeposit ? "ค่ามัดจำการจ้างงาน" : "ค่าตอบแทนการจ้างงาน"}
           </p>
-          <p>{getReal(props.amount)} บาท</p>
+          <p>{getReal(props.amount).toFixed(2)} บาท</p>
         </div>
         {!props.isStudent && (
           <>
             <div className="flex justify-between font-normal text-[14px] md:text-[16px] text-slate-500">
               <p>ค่าบริการ 15 %</p>
-              <p>{getCom(props.amount)} บาท</p>
+              <p>{getCom(props.amount).toFixed(2)} บาท</p>
             </div>
             <div className="flex justify-between font-normal text-[14px] md:text-[16px] text-slate-700">
               <p>ยอดชำระทั้งหมด</p>
-              <p>{props.amount} บาท</p>
+              <p>{(props.amount).toFixed(2)} บาท</p>
             </div>
           </>
         )}
