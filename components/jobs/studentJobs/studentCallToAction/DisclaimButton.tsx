96a4e9104d341e8bc67034af4d7d839344bcc57e
Sarunpong Lapsuksathit <6432163521@student.chula.ac.th>
2024-03-31 21:56:16 +0700

    Make text on buttons centered

--- components/jobs/studentJobs/studentCallToAction/DisclaimButton.tsx
+++ components/jobs/studentJobs/studentCallToAction/DisclaimButton.tsx
@@ -11,7 +11,7 @@ export default function DisclaimButton({ jobId }: { jobId: string }) {
     return (
         <div className="flex flex-row justify-between w-full gap-1">
             <DangerButton
-                className="py-2 px-3 w-full bg-red-500 text-sm text-white rounded-md hover:shadow-md hover:bg-red-600 active:bg-red-700 transition duration-200 ease-in-out"
+                className="flex justify-center items-center py-2 px-3 w-full bg-red-500 text-sm text-white rounded-md hover:shadow-md hover:bg-red-600 active:bg-red-700 transition duration-200 ease-in-out"
                 isLoading={isLoading}
                 isDisabled={isDisabled}
                 onClick={async () => {
--- components/jobs/studentJobs/studentCallToAction/SubmitTaskButton.tsx
+++ components/jobs/studentJobs/studentCallToAction/SubmitTaskButton.tsx
@@ -15,25 +15,23 @@ export default function SubmitTaskButton({ jobId, employerId }: { jobId: string,
               employerId={employerId}
               className="flex flex-row justify-center items-center py-2 px-3 w-full bg-slate-50 text-sm text-slate-900 border border-slate-700 rounded-md hover:shadow-md hover:border-slate-800 active:border-slate-900 transition duration-200 ease-in-out"
           />
-        
-            <PrimaryButton
-            className="py-2 px-3 w-full bg-slate-600 text-sm text-white rounded-md hover:shadow-md hover:bg-slate-700 active:bg-slate-800 transition duration-200 ease-in-out"
-            isLoading={isLoading}
-            isDisabled={isDisabled}
-            onClick={async () => {
-                setLoading(true);
-                setDisabled(true);
-                await inProgressToDelivered(jobId);
-                setLoading(false);
-                setDisabled(false);
-                toast.success("ส่งมอบงานแล้ว");
-                location.reload();
-            }}
 
-            >
-            ส่งมอบงาน
-            </PrimaryButton>
-
+          <PrimaryButton
+              className="flex justify-center items-center py-2 px-3 w-full bg-slate-600 text-sm text-white rounded-md hover:shadow-md hover:bg-slate-700 active:bg-slate-800 transition duration-200 ease-in-out"
+              isLoading={isLoading}
+              isDisabled={isDisabled}
+              onClick={async () => {
+                  setLoading(true);
+                  setDisabled(true);
+                  await inProgressToDelivered(jobId);
+                  setLoading(false);
+                  setDisabled(false);
+                  toast.success("ส่งมอบงานแล้ว");
+                  location.reload();
+              }}
+          >
+              ส่งมอบงาน
+          </PrimaryButton>
       </div>
   );
 }
