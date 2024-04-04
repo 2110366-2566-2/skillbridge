8456e343b844cfc865acde02300d728125b3bc95
Fei-7 <supachokang@gmail.com>
2024-04-05 00:32:55 +0700

    fix : review button x-axis padding

--- components/jobs/employerJobs/employerCallToAction/DoneButton.tsx
+++ components/jobs/employerJobs/employerCallToAction/DoneButton.tsx
@@ -21,12 +21,12 @@ export default function DoneButton({
             <ProgressButton jobId={jobId} studentId={studentId} />
             {!isReviewed ? 
                 <PrimaryButton 
-                    className="w-[83px] h-[36px] px-[1px] py-[8px]"
+                    className="w-[83px] h-[36px] px-0 py-[8px]"
                     onClick={reviewButtonOnClick}
                 >รีวิวนิสิต</PrimaryButton>
                 : 
                 <PrimaryButton 
-                    className="w-[83px] h-[36px] px-[1px] py-[8px]"
+                    className="w-[83px] h-[36px] px-0 py-[8px]"
                     isDisabled={true}
                 >
                     รีวิวแล้ว
