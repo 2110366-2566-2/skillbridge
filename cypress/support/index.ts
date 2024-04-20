1feddd4c187524fc5cb5f4266c775ead69c64565
Awat Singdamrong <a.lubing@yahoo.com>
2024-04-20 22:01:01 +0700

    delete: All cypress

--- cypress/support/index.ts
+++
@@ -1,9 +0,0 @@
-declare namespace Cypress {
-    interface Chainable {
-        /**
-         * Custom command to select DOM element by data-cy attribute.
-         * @example cy.dataCy('greeting')
-         */
-        loginAsStudent(email: string, password: string): Chainable<Element>;
-    }
-}
